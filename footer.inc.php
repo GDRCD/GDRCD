165cf32e5f2a0e40a92493dd346494f8d74804cb
BreakerYouko <breakeryouko@gmail.com>
2015-02-02 12:27:06 +0100

    Added function modalWindow()

--- footer.inc.php
+++ footer.inc.php
@@ -42,7 +42,31 @@ if ($PARAMETERS['mode']['popup_choise']=='ON')
 <?php
 }
 ?>
+<script type="text/javascript">
 
+function modalWindow(name, title, url, width, height) {
+    // per width ed height imposto dei valori di default così non occorre specificarli in ogni occasione
+    width = typeof width === 'undefined'? 800 : width;
+    height = typeof height === 'undefined'? 600 : height;
+    
+    // verifichiamo se nel body non esiste il sorgente per la dialog
+    if ($('#dialog-'+name).length == 0) {
+    
+        // in questo caso lo creiamo:
+        $('body').append('<div id="dialog-'+name+'" title="'+title+'" style="padding:0;"><iframe src="'+ url +'" frameborder="no" style="position:absolute;width:100%;height:100%;" scrolling="yes"></div>');
+    
+    } else {
+        
+        // se il sorgente invece esiste già assegnamo la nuova url all´iframe:
+        $('#dialog-'+name).attr('title', title);
+        $('#dialog-'+name+' iframe').attr('src', url);
+    }
+    
+    // Ok, adesso siamo pronti per lanciare la modale!
+    $('#dialog-'+name).dialog({width: width, height: height});
+}
+
+</script>
 </body>
 </html><?php
 
