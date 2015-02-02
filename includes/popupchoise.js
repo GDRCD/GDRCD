469b6fd65ea10cd77ab7dad0dbf740d08c35a288
BreakerYouko <breakeryouko@gmail.com>
2015-02-02 12:13:54 +0100

    Remove jQuery conflict
    sostituito $ con gdrcd_selector

--- includes/popupchoise.js
+++ includes/popupchoise.js
@@ -5,16 +5,16 @@ function check_login()
 {
 	if (!gdrcd_selector('allow_popup').checked)
 	{
-		$('popup').value = '0';
-		$('do_login').target = '_top';
-		$('do_login').submit();
+		gdrcd_selector('popup').value = '0';
+		gdrcd_selector('do_login').target = '_top';
+		gdrcd_selector('do_login').submit();
 	
 	}else
 	{
 		window.open('', 'MYGDR', 'toolbar=0,menubar=0,directories=0,location=0,scrollbars=yes,status=0,resizable=1');
 		
-		$('popup').value = '1';
-		$('do_login').target = 'MYGDR';
-		$('do_login').submit();
+		gdrcd_selector('popup').value = '1';
+		gdrcd_selector('do_login').target = 'MYGDR';
+		gdrcd_selector('do_login').submit();
 	}
 }
