ff447947327dc1c8a45de5053d951d42b9f664f8
BreakerYouko <breakeryouko@gmail.com>
2015-02-05 17:21:53 +0100

    added __DIR__

--- includes/required.php
+++ includes/required.php
@@ -1,8 +1,8 @@
 <?php
   
-  require_once('../config.inc.php');
-	require_once('constant_values.inc.php');
-	require_once('..(vocabulary/'.$PARAMETERS['languages']['set'].'.vocabulary.php');
- 	require_once('functions.inc.php');
+	require_once(__DIR__.'/../config.inc.php');
+	require_once(__DIR__.'/constant_values.inc.php');
+	require_once(__DIR__.'/../vocabulary/'.$PARAMETERS['languages']['set'].'.vocabulary.php');
+ 	require_once(__DIR__.'/functions.inc.php');
 
 ?>
