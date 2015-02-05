aef54879e46942ad006ed50215a1165501544ebf
BreakerYouko <breakeryouko@gmail.com>
2015-02-05 15:27:34 +0100

    Create required.php
    Creato un file per non dover includere ogni volta tutti i file necessari.

---
+++ includes/required.php
@@ -0,0 +1,8 @@
+<?php
+  
+  require_once('../config.inc.php');
+	require_once('constant_values.inc.php');
+	require_once('..(vocabulary/'.$PARAMETERS['languages']['set'].'.vocabulary.php');
+ 	require_once('functions.inc.php');
+
+?>
