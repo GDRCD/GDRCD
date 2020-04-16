643ee1e19ec54c0b35d72076024fbe9928e41c37
BreakerYouko <breakeryouko@gmail.com>
2020-04-16 22:38:34 +0200

    refactoring index

--- index.php
+++ index.php
@@ -5,66 +5,60 @@ $check_for_update = false;
 require 'header.inc.php';
 require 'includes/credits.inc.php';
 
-if ($PARAMETERS['settings']['protection'] == 'ON')
-{
+if ($PARAMETERS['settings']['protection'] == 'ON'){
     require 'protezione.php';
 }
 
-/** * Definizione pagina da visualizzare */
+/*
+ * Definizione pagina da visualizzare
+ */
 $page = ( ! empty($_GET['page'])) ? gdrcd_filter('include', $_GET['page']) : 'index';
 
-/** * Definizione dell'eventuale contenuto interno
-* Utile se si vuol mantenere la struttura della homepage quando si aprono i link
-*/
-if ( ! empty($_GET['content']))
-{
-    $content = gdrcd_filter('include', $_GET['content']);
-} else
-{
-    $content = 'home';
-}
+/*
+ * Definizione dell'eventuale contenuto interno
+ * Utile se si vuol mantenere la struttura della homepage quando si aprono i link
+ */
+$content = ( ! empty($_GET['content'])) ? gdrcd_filter('include', $_GET['content']) : 'home';
 
-/** * Algoritmi di base della homepage
-*/
-
-/** * Conteggio utenti online
-*/
+/*
+ * Conteggio utenti online
+ */
 $users = gdrcd_query("SELECT COUNT(nome) AS online FROM personaggio WHERE ora_entrata > ora_uscita AND DATE_ADD(ultimo_refresh, INTERVAL 4 MINUTE) > NOW()");
 
-/** * Procedura di recupero Password
-*/
+/*
+ * Procedura di recupero Password
+ */
 $RP_response = '';
 
 if ( ! empty($_POST['email'])) {
     $newpass = gdrcd_query("SELECT email FROM personaggio WHERE email = '" . gdrcd_filter('in',
         $_POST['email']) . "' LIMIT 1", 'result');
 
-        if (gdrcd_query($newpass, 'num_rows') > 0)
-        {
-            gdrcd_query($newpass, 'free');
+    if (gdrcd_query($newpass, 'num_rows') > 0) {
+        gdrcd_query($newpass, 'free');
 
-            $pass = gdrcd_genera_pass();
-            gdrcd_query("UPDATE personaggio SET pass = '" . gdrcd_encript($pass) . "' WHERE email = '" . gdrcd_filter('in',
-            $_POST['email']) . "' LIMIT 1");
+        $pass = gdrcd_genera_pass();
+        gdrcd_query("UPDATE personaggio SET pass = '" . gdrcd_encript($pass) . "' WHERE email = '" . gdrcd_filter('in',
+        $_POST['email']) . "' LIMIT 1");
 
-            $subject = gdrcd_filter('out',
-            $MESSAGE['register']['forms']['mail']['sub'] . ' ' . $PARAMETERS['info']['site_name']);
-            $text = gdrcd_filter('out', $MESSAGE['register']['forms']['mail']['text'] . ': ' . $pass);
+        $subject = gdrcd_filter('out',
+        $MESSAGE['register']['forms']['mail']['sub'] . ' ' . $PARAMETERS['info']['site_name']);
+        $text = gdrcd_filter('out', $MESSAGE['register']['forms']['mail']['text'] . ': ' . $pass);
 
-            mail($_POST['email'], $subject, $text, 'From: ' . $PARAMETERS['info']['webmaster_email']);
+        mail($_POST['email'], $subject, $text, 'From: ' . $PARAMETERS['info']['webmaster_email']);
 
+        $RP_response = gdrcd_filter('out', $MESSAGE['warning']['modified']);
 
-            $RP_response = gdrcd_filter('out', $MESSAGE['warning']['modified']);
-
-        } else
-        {
-            $RP_response = gdrcd_filter('out', $MESSAGE['warning']['cant_do']);
-        }
-
+    } else {
+        $RP_response = gdrcd_filter('out', $MESSAGE['warning']['cant_do']);
     }
-    /** * Fine Recupero Password */
 
-    include 'themes/' . $PARAMETERS['themes']['current_theme'] . '/home/' . $page . '.php';
+}
+/*
+ * Fine Recupero Password
+ */
 
-    require 'footer.inc.php';
-    ?>
+include 'themes/' . $PARAMETERS['themes']['current_theme'] . '/home/' . $page . '.php';
+
+require 'footer.inc.php';
+?>
