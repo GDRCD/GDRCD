bf0efa59b28cf4bf561bb9e5ab5abacf35619676
Kasui92 <pattocchioluca@gmail.com>
2021-09-04 14:15:31 +0200

    Forum [#154] - Ora gli utenti possono visualizzare solo i Forum a cui sono abilitati.

--- pages/forum/index.inc.php
+++ pages/forum/index.inc.php
@@ -10,25 +10,30 @@ $ultimotipo = -1;
         <table>
             <?php
             while($row = gdrcd_query($result, 'fetch')) {
+
+                // Controllo i permessi di visualizzazione del forum
+                if((($row['tipo'] == SOLORAZZA) && ($_SESSION['id_razza'] != $row['proprietari']) && ($_SESSION['permessi'] < MODERATOR)) || (($row['tipo'] == SOLOGILDA) && (strpos($_SESSION['gilda'], '*'.$row['proprietari'].'*') === false) && ($_SESSION['permessi'] < MODERATOR)) || (($row['tipo'] >= SOLOMASTERS) && ($_SESSION['permessi'] < GAMEMASTER)) || (($row['tipo'] >= SOLOMODERATORS) && ($_SESSION['permessi'] < MODERATOR))) {
+                    // Se non possiedo i permessi, non mostro la sezione
+                    continue;
+                }
+
                 if($row['tipo'] != $ultimotipo) {
                     /*Sono ordinati per tipo, se cambia stampo il nuovo tipo come capoverso*/
                     $ultimotipo = $row['tipo'];
-
-                    if(($row['tipo'] != SOLORAZZA || $_SESSION['id_razza'] == $row['proprietari'] || $_SESSION['permessi'] >= MODERATOR) && ($row['tipo'] != SOLOGILDA || strpos($_SESSION['gilda'],'*'.$row['proprietari'].'*') !== false || $_SESSION['permessi'] >= MODERATOR) && ($row['tipo'] < SOLOMASTERS || $_SESSION['permessi'] >= GAMEMASTER) && ($row['tipo'] < SOLOMODERATORS || $_SESSION['permessi'] >= MODERATOR)) {
-                        ?>
-                        <tr><!-- Intestazione tabella -->
-                            <td colspan="2">
-                                <div class="capitolo_elenco">
-                                    <?php echo gdrcd_filter('out', $PARAMETERS['names']['forum']['plur'].' '.strtolower($MESSAGE['interface']['forums']['type'][$ultimotipo])); ?>
-                                </div>
-                            </td>
-                        </tr>
-                        <?php
-                    } //if permessi
+                    ?>
+                    <tr><!-- Intestazione tabella -->
+                        <td colspan="2">
+                            <div class="capitolo_elenco">
+                                <?php echo gdrcd_filter('out', $PARAMETERS['names']['forum']['plur'].' '.strtolower($MESSAGE['interface']['forums']['type'][$ultimotipo])); ?>
+                            </div>
+                        </td>
+                    </tr>
+                    <?php
                 } //if
-                $new_msg = gdrcd_query("SELECT COUNT(id) AS num FROM araldo_letto WHERE araldo_id = ".$row['id_araldo']." AND nome = '".$_SESSION['login']."';");
 
+                $new_msg = gdrcd_query("SELECT COUNT(id) AS num FROM araldo_letto WHERE araldo_id = ".$row['id_araldo']." AND nome = '".$_SESSION['login']."';");
                 $new_msg2 = gdrcd_query("SELECT COUNT(id_messaggio) AS num FROM messaggioaraldo WHERE id_araldo = ".$row['id_araldo']." AND id_messaggio_padre = -1");
+
                 ?>
                 <tr><!-- Forum della categoria -->
                     <td class="forum_main_post_author">
@@ -42,19 +47,9 @@ $ultimotipo = -1;
                     </td>
                     <td class="casella_elemento">
                         <div class="elementi_elenco">
-                            <?php
-                            if(($row['tipo'] <= PERTUTTI) || (($row['tipo'] == SOLORAZZA) && ($_SESSION['id_razza'] == $row['proprietari'])) || (($row['tipo'] == SOLOGILDA) && (strpos($_SESSION['gilda'],'*'.$row['proprietari'].'*') != false)) || (($row['tipo'] == SOLOMASTERS) && ($_SESSION['permessi'] >= GAMEMASTER)) || ($_SESSION['permessi'] >= MODERATOR)){
-                            /*Restrizione di visualizzazione solo master e admin*/
-                            ?>
-                                <a href="main.php?page=forum&op=visit&what=<?php echo gdrcd_filter('out', $row['id_araldo']); ?>&name=<?php echo gdrcd_filter('out', $row['nome']); ?>">
-                            <?php
-                            }
-                            echo gdrcd_filter('out', $row['nome']);
-
-                            if(($row['tipo'] <= PERTUTTI) || (($row['tipo'] == SOLORAZZA) && ($_SESSION['id_razza'] == $row['proprietari'])) || (($row['tipo'] == SOLOGILDA) && (strpos($_SESSION['gilda'],'*'.$row['proprietari'].'*') != false)) || (($row['tipo'] == SOLOMASTERS) && ($_SESSION['permessi'] >= GAMEMASTER)) || ($_SESSION['permessi'] >= MODERATOR)){ /*Restrizione di visualizzazione solo master e admin*/
-                                echo '</a>';
-                            }//if
-                            ?>
+                            <a href="main.php?page=forum&op=visit&what=<?php echo gdrcd_filter('out', $row['id_araldo']); ?>&name=<?php echo gdrcd_filter('out', $row['nome']); ?>">
+                                <?php echo gdrcd_filter('out', $row['nome']); ?>
+                            </a>
                         </div>
                     </td>
                 </tr>
