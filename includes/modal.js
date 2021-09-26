82eff5b58e20d432a9bff19b49bc4c64c144c749
Kasui92 <pattocchioluca@gmail.com>
2021-09-26 12:08:43 +0200

    GDRCD [Fix] - Inibito autofocus alla prima apertura di una modale.

--- includes/modal.js
+++ includes/modal.js
@@ -6,7 +6,13 @@ function modalWindow(name, title, url, width, height) {
     // verifichiamo se nel body non esiste il sorgente per la dialog
     if (top.$('#dialog-'+name).length === 0) {
         // in questo caso lo creiamo:
-        top.$('body').append('<div id="dialog-'+name+'" title="'+title+'" style="padding:0;"><iframe src="'+ url +'" frameborder="no" style="position:absolute;width:100%;height:100%;" scrolling="yes"></div>');
+        top.$('body').append(
+            '<div id="dialog-'+name+'" title="'+title+'" style="padding:0;">' +
+                    // Utilizzo un input invisibile per disabilitare l'autofocus di jQuery
+                    '<span class="ui-helper-hidden-accessible"><input type="text"/></span>' +
+                    // Contenuto della Modale
+                    '<iframe src="'+ url +'" frameborder="no" style="position:absolute;width:100%;height:100%;" scrolling="yes">' +
+                    '</div>');
     } else {
         // se il sorgente invece esiste già assegnamo la nuova url all´iframe:
         top.$('#dialog-'+name).attr('title', title);
