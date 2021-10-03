6aef0f8f364bdd4f353515f2167cdc5986106668
Luca Pattocchio <pattocchioluca@gmail.com>
2021-10-03 14:28:12 +0200

    MESSAGGI [FIX]: Aggiornato il metodo di invio dei messaggi.

--- pages/messages/send_message.inc.php
+++ pages/messages/send_message.inc.php
@@ -1,59 +1,113 @@
 <?php
-if (gdrcd_filter('get', $_POST['multipli']) == 'singolo') {
-    $check_dest = explode(',', gdrcd_filter('get', $_POST['destinatario']));
-    $destinat = trim($check_dest[0]);
-    if (gdrcd_filter('get', $_POST['url']) != "") {
-        $_POST['testo'] = $_SESSION['login'] . ' ti ha segnalato questo [url=' . $_POST['url'] . ']link[/url].';
-    }//if
 
-    $result = gdrcd_query("SELECT nome FROM personaggio WHERE nome = '" . $destinat . "'", 'result');
-    if ((gdrcd_query($result, 'num_rows') > 0) && (empty($destinat) === false)) {
-        gdrcd_query("INSERT INTO messaggi (mittente, destinatario, spedito, testo) VALUES ('" . $_SESSION['login'] . "', '" . gdrcd_capital_letter(gdrcd_filter('in', $destinat)) . "', NOW(), '" . gdrcd_filter('in', $_POST['testo']) . "')");
-        gdrcd_query("INSERT INTO backmessaggi (mittente, destinatario, spedito, testo) VALUES ('" . $_SESSION['login'] . "', '" . gdrcd_capital_letter(gdrcd_filter('in', $destinat)) . "', NOW(), '" . gdrcd_filter('in', $_POST['testo']) . "')");
-    }//if
-} else {
-    if ($_POST['multipli'] == 'presenti') {
-        $query = "SELECT personaggio.nome, personaggio.cognome, personaggio.permessi, personaggio.sesso, personaggio.id_razza, razza.sing_m, razza.sing_f, razza.icon, personaggio.disponibile, personaggio.online_status, personaggio.is_invisible, personaggio.ultima_mappa, personaggio.ultimo_luogo, personaggio.posizione, personaggio.ora_entrata, personaggio.ora_uscita, personaggio.ultimo_refresh, mappa.stanza_apparente, mappa.nome as luogo, mappa_click.nome as mappa FROM personaggio LEFT JOIN mappa ON personaggio.ultimo_luogo = mappa.id LEFT JOIN mappa_click ON personaggio.ultima_mappa = mappa_click.id_click LEFT JOIN razza ON personaggio.id_razza = razza.id_razza WHERE personaggio.ora_entrata > personaggio.ora_uscita AND DATE_ADD(personaggio.ultimo_refresh, INTERVAL 4 MINUTE) > NOW() ORDER BY personaggio.is_invisible, personaggio.ultima_mappa, personaggio.ultimo_luogo, personaggio.nome";
+/**
+ * In base alla tipologia di richiesta, eseguo le relative operazioni di invio messaggio
+ *
+ * La tipologia di richiesta viene determinata all'interno di $_POST['multipli'],
+ * per aggiungere nuove richieste è sufficiente integrare un nuovo caso nello switch
+ */
+
+// Ottengo la richiesta
+$opRequest = gdrcd_filter('get', $_POST['multipli']);
+
+switch($opRequest) {
+    /**
+     * INVIO MESSAGGIO A SINGOLO
+     * ATTENZIONE: nel caso vengano individuati più destinatari (separati da virgola),
+     * verrà comunque inviato il messaggio solo al primo individuo
+     */
+    default:
+    case 'singolo':
+        // Ottengo il destinatario, prendo il primo inviato
+        $destinatari = explode(',', gdrcd_filter('get', $_POST['destinatario']));
+        $destinatario = trim($destinatari[0]);
+
+        // In caso di segnalazione
+        if (gdrcd_filter('get', $_POST['url']) != "") {
+            $_POST['testo'] = $_SESSION['login'] . ' ti ha segnalato questo [url=' . $_POST['url'] . ']link[/url].';
+        }
+
+        // Determino se il Personaggio a cui sto inviando esiste
+        $result = gdrcd_query("SELECT nome FROM personaggio WHERE nome = '" . $destinatario . "'", 'result');
+        if ((gdrcd_query($result, 'num_rows') > 0) && (empty($destinatario) === false)) {
+            // Inserisco i messaggi
+            gdrcd_query("INSERT INTO messaggi (mittente, destinatario, spedito, oggetto, testo) VALUES ('" . $_SESSION['login'] . "', '" . gdrcd_capital_letter(gdrcd_filter('in', $destinatario)) . "', NOW(), '" . gdrcd_filter('in', $_POST['otteggo']) . "', '" . gdrcd_filter('in', $_POST['testo']) . "')");
+            gdrcd_query("INSERT INTO backmessaggi (mittente, destinatario, spedito, oggetto, testo) VALUES ('" . $_SESSION['login'] . "', '" . gdrcd_capital_letter(gdrcd_filter('in', $destinatario)) . "', NOW(), '" . gdrcd_filter('in', $_POST['otteggo']) . "', '" . gdrcd_filter('in', $_POST['testo']) . "')");
+        }
+        break;
+
+    /**
+     * INVIO MESSAGGIO A TUTTI I PRESENTI
+     */
+    case 'presenti':
+        $query = "SELECT personaggio.nome 
+                    FROM personaggio 
+                    WHERE personaggio.ora_entrata > personaggio.ora_uscita 
+                      AND DATE_ADD(personaggio.ultimo_refresh, INTERVAL 4 MINUTE) > NOW()";
         $result = gdrcd_query($query, 'result');
 
+        // Scorro tutti i presenti individuati
         while ($record = gdrcd_query($result, 'fetch')) {
-            gdrcd_query("INSERT INTO messaggi (mittente, destinatario, spedito, testo) VALUES ('" . $_SESSION['login'] . "', '" . $record['nome'] . "', NOW(), '" . gdrcd_filter('in', $_POST['testo']) . "')");
+            gdrcd_query("INSERT INTO messaggi (mittente, destinatario, spedito, oggetto, testo) VALUES ('" . $_SESSION['login'] . "', '" . $record['nome'] . "', NOW(), '" . gdrcd_filter('in', $_POST['oggetto']) . "', '" . gdrcd_filter('in', $_POST['testo']) . "')");
         }
-    } elseif ($_POST['multipli'] == 'multiplo') {
-        $check_dest = explode(',', $_POST['destinatario']);
-        $query = "INSERT INTO messaggi (mittente, destinatario, spedito, testo) VALUES";
-        foreach ($check_dest as $destinat) {
-            $destinat = trim($destinat);
+        break;
 
-            $result = gdrcd_query("SELECT nome FROM personaggio WHERE nome = '" . gdrcd_filter('in', $destinat) . "'", 'result');
-            if (gdrcd_query($result, 'num_rows') > 0) {
-                $query .= " ('" . $_SESSION['login'] . "', '" . gdrcd_capital_letter(gdrcd_filter('in', $destinat)) . "', NOW(), '" . gdrcd_filter('in', $_POST['testo']) . "'),";
-            }
-        }
-        $query = substr($query, 0, -1);
-        gdrcd_query($query);
-        /** * Bugfix: commentato la stampa della variabile $query. In caso di messaggio multiplo stampava
-         * l'ultima query eseguita.
-         * @author Rhllor
+    /**
+     * INVIO MESSAGGIO MULTIPLO
+     * I destinatari vengono separati da virgola
+     */
+    case 'multiplo':
+        // Ottengo i destinatari
+        $destinatari = explode(',', $_POST['destinatario']);
+
+        /**
+         * Controllo che i destinatari siano effettivamente dei personaggi
+         * I personaggi vengono concatenati nel seguente formato:
+         *
+         * 'NOMEPERSONAGGIO','NOMEPERSONAGGIO2'
+         *
+         * in modo che poi possano essere inseriti nel controllo IN.
+         * Aggiungo che il campo personaggio.nome non sia NULL, per evitare possibili errori.
          */
-    } elseif ($_POST['multipli'] == "broadcast") {
-        $personaggio = gdrcd_query("SELECT * FROM personaggio where nome = '" . $_SESSION['login'] . "'");
+        //
+        $destinatariCheck = "'".implode("','", $destinatari)."'";
+        $result = gdrcd_query("SELECT nome FROM personaggio WHERE nome IN (" . $destinatariCheck . ") AND nome IS NOT NULL", 'result');
 
-        if($personaggio['permessi'] >= MODERATOR) {
-            $query = gdrcd_query("SELECT nome FROM personaggio", 'result');
-            while ($row = gdrcd_query($query, 'fetch'))
-            {
-                gdrcd_query("INSERT INTO messaggi (mittente, destinatario, spedito, testo) VALUES ('" . $_SESSION['login'] . "', '" . $row['nome'] . "' , NOW(), '" . gdrcd_filter('in', $_POST['testo']) . "')");
+        // Se sono stati individuati dei personaggi, proseguo con l'invio dei messaggi
+        if(gdrcd_query($result, 'num_rows') > 0){
+            // Scorro tutti i personaggi
+            while ($record = gdrcd_query($result, 'fetch')) {
+                // Creo l'inserimento
+                $queryInsert[] = "('" . $_SESSION['login'] . "', '" . $record['nome'] . "', NOW(), '" . gdrcd_filter('in', $_POST['oggetto']) . "', '" . gdrcd_filter('in', $_POST['testo']) . "')";
+            }
+
+            // Se ho costruito delle query di inserimento, prevedo la query
+            if(isset($queryInsert)){
+                $query = gdrcd_query("INSERT INTO messaggi (mittente, destinatario, spedito, oggetto, testo) VALUES ".implode(",", $queryInsert));
+                $query = gdrcd_query("INSERT INTO backmessaggi (mittente, destinatario, spedito, oggetto, testo) VALUES ".implode(",", $queryInsert));
+                gdrcd_query($query, 'free');
             }
         }
-        gdrcd_query($query, 'free');
-    } elseif (is_numeric($_POST['multipli']) === true) {
-        gdrcd_query("INSERT INTO messaggi (mittente, destinatario, spedito, testo) VALUES ('" . $_SESSION['login'] . "', '" . $_POST['multipli'] . "', NOW(), '" . gdrcd_filter('in', $_POST['testo']) . "')");
-    } elseif (empty($_POST['destinatario']) === false) {
-        gdrcd_query("INSERT INTO messaggi (mittente, destinatario, spedito, testo) VALUES ('" . $_SESSION['login'] . "', '" . gdrcd_capital_letter(gdrcd_filter('in', $_POST['destinatario'])) . "', NOW(), '" . gdrcd_filter('in', $_POST['testo']) . "')");
-        gdrcd_query("INSERT INTO backmessaggi (mittente, destinatario, spedito, testo) VALUES ('" . $_SESSION['login'] . "', '" . gdrcd_capital_letter(gdrcd_filter('in', $_POST['destinatario'])) . "', NOW(), '" . gdrcd_filter('in', $_POST['testo']) . "')");
-    }
-}//else
+        break;
+
+
+    /**
+     * INVIO MESSAGGIO A TUTTI
+     */
+    case 'broadcast':
+        // Controllo sui permessi dell'utente
+        if($_SESSION['permessi'] >= MODERATOR) {
+            // Ottengo tutti i personaggi e li scorro per l'invio del messaggio
+            $query = gdrcd_query("SELECT nome FROM personaggio", 'result');
+            while ($row = gdrcd_query($query, 'fetch')) {
+                // Creo l'inserimento
+                gdrcd_query("INSERT INTO messaggi (mittente, destinatario, spedito, oggetto, testo) VALUES ('" . $_SESSION['login'] . "', '" . $row['nome'] . "' , NOW(), '" . gdrcd_filter('in', $_POST['oggetto']) . "', '" . gdrcd_filter('in', $_POST['testo']) . "')");
+            }
+            gdrcd_query($query, 'free');
+        }
+        break;
+}
+
 ?>
     <div class="warning">
         <?php echo $PARAMETERS['names']['private_message']['sing'] . $MESSAGE['interface']['messages']['sent']; ?>
