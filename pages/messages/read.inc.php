6967a6e0fd4065a6b04f312f7e6c7e243fa8bd3b
Luca Pattocchio <pattocchioluca@gmail.com>
2021-10-03 23:54:27 +0200

    MESSAGGI [UPD]: Aggiornata la visualizzazione dei messaggi.

--- pages/messages/read.inc.php
+++ pages/messages/read.inc.php
@@ -1,11 +1,9 @@
 <?php
-/** * Bugfix: correzione di un bug che permetteva la visualizzazione di messaggi non inviati all'utente
- * semplicemente modificando l'id. Viene quindi aggiunta nella clausola where il controllo sulla proprietà
- * del messaggio. Nel caso in cui non venga trovato alcun messaggio verrà mostrato un errore.
- * @author Rhllor
- */
-//$result=gdrcd_query("SELECT * FROM messaggi WHERE id = ".gdrcd_filter('num',$_REQUEST['id_messaggio'])." LIMIT 1", 'result');
+
+// Ottengo i dati del messaggio
 $result = gdrcd_query("SELECT * FROM messaggi WHERE id = ".gdrcd_filter('num', $_REQUEST['id_messaggio'])." and ( destinatario = '".$_SESSION['login']."' or mittente = '".$_SESSION['login']."') LIMIT 1", 'result');
+
+// Se non ottengo alcun risultato, allora mostro un messaggio di errore
 if(gdrcd_query($result, 'num_rows') == 0) { ?>
     <div class="warning">
         Impossibile visualizzare il messaggio richiesto, il messaggio potrebbe non esistere oppure non
@@ -13,47 +11,43 @@ if(gdrcd_query($result, 'num_rows') == 0) { ?>
     </div>
     <?php
 } else {
+    // Preparo i dati
     $record = gdrcd_query($result, 'fetch');
     gdrcd_query($result, 'free');
-    //Leggi id messaggio
-    //Formatta messaggio
-    //Bottoni Rispondi, Rispondi e allega, cancella
+
+    // Aggiorno lo stato letto del messaggio
+    if(($record['destinatario'] == $_SESSION['login']) && ($record['letto'] == 0)) {
+        gdrcd_query("UPDATE messaggi SET letto = 1 WHERE id = ".gdrcd_filter('num', $record['id'])." LIMIT 1");
+    }
+
+    // Suddivido la data di invio
+    list($data_spedito, $ora_spedito) = explode(' ', $record['spedito']);
+
     ?>
     <div class="read_message_box">
         <div class="infos">
             <span class="title"><?php echo gdrcd_filter('out', $MESSAGE['interface']['messages']['date']).": "; ?></span>
-            <span class="body">
-			   <?php $quando = explode(' ', $record['spedito']);
-               echo gdrcd_format_date($quando[0]) ?>
- 			</span>
-            <span class="title">
- 			       <?php echo ' '.gdrcd_filter('out', $MESSAGE['interface']['messages']['time']).' '; ?>
- 			</span>
-            <span class="body">
- 				   <?php echo gdrcd_format_time($quando[1]); ?>
+            <span class="body"><?php echo gdrcd_format_date($data_spedito) ?></span>
+            <span class="title"><?php echo ' '.gdrcd_filter('out', $MESSAGE['interface']['messages']['time']).' '; ?></span>
+            <span class="body"><?php echo gdrcd_format_time($ora_spedito); ?>
  		    </span>
         </div>
         <div class="infos">
- 		    <span class="title"><?php echo gdrcd_filter('out', $MESSAGE['interface']['messages']['sender']).": "; ?>
- 			</span>
+ 		    <span class="title"><?php echo gdrcd_filter('out', $MESSAGE['interface']['messages']['sender']).": "; ?></span>
             <span class="body"><?php echo gdrcd_filter('out', $record['mittente']); ?></span>
         </div>
-        <?php if(($record['destinatario'] == $_SESSION['login']) && ($record['letto'] == 0)) {
-            gdrcd_query("UPDATE messaggi SET letto = 1 WHERE id = ".gdrcd_filter('num', $_REQUEST['id_messaggio'])." LIMIT 1");
-        } ?>
+        <div class="infos">
+            <span class="title"><?php echo gdrcd_filter('out', $MESSAGE['interface']['messages']['subject']).": "; ?></span>
+ 		    <span class="body"><?php echo gdrcd_filter('out', $record['oggetto']);?></span>
+        </div>
         <div class="read_message_box_text">
             <?php echo nl2br(gdrcd_bbcoder(gdrcd_filter('out', $record['testo']))); ?>
         </div>
 
         <div class="read_message_box_forms">
-
-            <div class="read_message_box_form">
-            </div>
-
             <div class="read_message_box_form">
                 <!-- attach -->
-                <form action="main.php?page=messages_center"
-                      method="post">
+                <form action="main.php?page=messages_center" method="post">
                     <input type="hidden" name="reply_dest" value="<?php echo $record['mittente']; ?>" />
                     <input type="hidden" name="testo" value="<?php echo gdrcd_filter('out', $MESSAGE['interface']['messages']['attachment'].$record['testo']); ?>" />
                     <input type="hidden" name="op" value="attach" />
@@ -73,8 +67,9 @@ if(gdrcd_query($result, 'num_rows') == 0) { ?>
         </div>
         <!-- read_message_box_form -->
     </div>
-    <div class="link_back">
-        <a href="main.php?page=messages_center&offset=0"><?php echo gdrcd_filter('out', $MESSAGE['interface']['messages']['go_back']); ?></a>
-    </div>
     <?php
-} // Chiudo controllo paternità messaggio
+}
+?>
+<div class="link_back">
+    <a href="main.php?page=messages_center&offset=0"><?php echo gdrcd_filter('out', $MESSAGE['interface']['messages']['go_back']); ?></a>
+</div>
