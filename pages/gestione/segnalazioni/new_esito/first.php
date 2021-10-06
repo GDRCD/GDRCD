a9e6617ede954e380a8e99896c8a7fcc83f0119c
Jansna <gd7j91r3>
2021-10-07 00:12:23 +0200

    Pacchetto esiti master [NEW]: Scambi di esiti e tiri fra master e giocatori.

---
+++ pages/gestione/segnalazioni/new_esito/first.php
@@ -0,0 +1,59 @@
+<?php
+if ($_GET['op']=='first') { ?>
+    <div class="page_title">
+        <h2>Nuova serie di esiti</h2>
+    </div>
+    <div class="form_info">
+        <? echo $MESSAGE['interface']['esitiserie']['intro'];?>
+    </div>
+    <form action="main.php?page=gestione_segnalazioni&segn=esito_index"
+          method="post"
+          class="form_gestione">
+
+        <div class='form_label'>
+            Nome PG coinvolto
+        </div>
+        <div class='form_field'>
+            <input name="pg" value=""/>
+        </div>
+
+        <div class='form_label'>
+            Titolo
+        </div>
+        <div class='form_field'>
+            <input name="titolo"
+                   value=""/>
+        </div>
+        <div class='form_label'>
+            Contenuto ON
+        </div>
+        <div class='form_field'>
+            <textarea name="contenuto"></textarea>
+        </div>
+
+        <div class="form_info" >
+            Indicazioni sulle azioni ON da compiere/compiute.
+        </div>
+
+        <div class='form_label'>
+            Note OFF
+        </div>
+        <div class='form_field'>
+            <input name="note" value="" />
+        </div>
+        <div class="form_info" >
+            Utilizzare solo per brevi chiarimenti
+        </div>
+        <!-- bottoni -->
+        <div class='form_submit'>
+            <input type="hidden"
+                   name="op"
+                   value="insert">
+            <input type="submit"
+                   value="<?php echo gdrcd_filter('out',$MESSAGE['interface']['forms']['submit']);?>" />
+        </div>
+
+    </form>
+<? }
+
+?>
