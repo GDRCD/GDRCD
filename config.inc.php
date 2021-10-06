12de39c2039afab1756c4f032521e18378629fb1
gianni10049 <38789905+gianni10049@users.noreply.github.com>
2021-10-06 14:35:42 +0200

    Riparato commit errato

--- config.inc.php
+++ config.inc.php
@@ -13,9 +13,9 @@
 error_reporting(E_ERROR | E_PARSE);
 
 /* PARAMETRI DI CONNESSIONE */
-$PARAMETERS['database']['username'] = 'provabea';            //nome utente del database
-$PARAMETERS['database']['password'] = '65NCyZVfYcRF';            //password del database
-$PARAMETERS['database']['database_name'] = 'my_provabea';    //nome del database
+$PARAMETERS['database']['username'] = 'gdrcd';            //nome utente del database
+$PARAMETERS['database']['password'] = 'gdrcd';            //password del database
+$PARAMETERS['database']['database_name'] = 'my_gdrcd';    //nome del database
 $PARAMETERS['database']['url'] = 'localhost';        //indirizzo ip del database
 
 
