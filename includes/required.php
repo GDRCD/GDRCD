7eaaa23f2542f4b686463946535d0fc50295c4fc
gianni10049 <38789905+gianni10049@users.noreply.github.com>
2021-10-10 00:13:19 +0200

    GDRCD [UPD] aggiunto file per l'override dei config in locale (#184)
    Authored-by: @ste-camp

--- includes/required.php
+++ includes/required.php
@@ -3,6 +3,10 @@ session_start();
 
 require_once(dirname(__FILE__) . '/constant_values.inc.php');
 require_once(dirname(__FILE__) . '/../config.inc.php');
+if(file_exists(dirname(__FILE__).'/config-overrides.php')){
+    include_once dirname(__FILE__).'/config-overrides.php';
+}
+
 require_once(dirname(__FILE__) . '/../vocabulary/' . $PARAMETERS['languages']['set'] . '.vocabulary.php');
 require_once(dirname(__FILE__) . '/functions.inc.php');
 
