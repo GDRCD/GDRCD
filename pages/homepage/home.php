1525844426636cfafda319965669c309cb408c3e
Luca Pattocchio <pattocchioluca@gmail.com>
2021-10-12 18:08:07 +0200

    HOME [FIX] Aggiornato il messaggio della pagina iniziale

--- pages/homepage/home.php
+++ pages/homepage/home.php
@@ -1,70 +1,47 @@
 <h2><?php echo $MESSAGE['homepage']['main_content']['welcome']; ?></h2>
 <p>
-    GDRCD5.6 è un upgrade della versione 5.5, sviluppato da vari programmatori in un progetto comunitario gestito da:
-    - gianni10049 (https://github.com/gianni10049)
-    - Kasui92 (https://github.com/kasui92).
-
-    Lato tecnico e sviluppo hanno contribuito:
-    - Anneth (https://github.com/annamereu)
-    - Jansna (https://github.com/Jansna)
-    - Leoblacksoul (https://github.com/ste-camp)
-    - Dyrr (Github)
-
-    Lato grafico hanno contribuito:
-    - Evolve (https://www.deviantart.com/tuffi8472)
-    - Nightspire
-    - Sara
-
-    Lato tester hanno contribuito:
-    - Musicamusa
-    - Jariel
-    - Zorya
+    <strong>GDRCD</strong> è un software CMS (Content Management System) per la realizzazione di browser game di genere "gioco di ruolo play by chat",
+    realizzato nella sua versione da un progetto di <strong>Romeo Gentile</strong> nel 2004 e mantenuto nel tempo da diversi autori che ne hanno permesso la sua diffusione.
 </p>
 <p>
-<p>
-    GDRCD5.5 è un upgrade della versione 5.4, sviluppato da Breaker.
+    Stai utilizzando la versione <strong><?=$PARAMETERS['info']['GDRCD']?></strong> di GDRCD, sviluppata dal <a href="https://github.com/orgs/GDRCD/people" target="_blank">Team di GDRCD</a>
+    sulla base del lavoro fatto da <strong>Salvatore (Blancks) Rotondo</strong> e <strong>breaker</strong> con <strong>GDRCD 5.1</strong>.
 </p>
 <p>
-    GDRCD5.4, nome in codice "Optimus", è un upgrade della versione 5.3, sviluppato da Breaker.
-</p>
-<p>
-    GDRCD5.3, nome in codice "Stable", è un upgrade della versione 5.2, sviluppato da Breaker.
-</p>
-<p>
-    GDRCD5.2, nome in codice "Stable", è un upgrade di versione di Blancks's GDRCD5.1, sviluppato da Breaker.
-</p>
-<p>
-    GDRCD5.1, nome in codice "Advanced", è un upgrade di versione di <a
-            href="http://www.gdr-online.com/download_dettaglio.asp?id=197">MrFaber's GDRCD5</a>, sviluppato da Salvatore
-   "Blancks" Rotondo.
-</p>
-<p>
-    Include un innumerevole quantitativo di bug fix e una migliore ottimizzazione del prodotto per una maggiore velocità,
-   stabilità ma soprattutto una maggiore <strong>sicurezza</strong>, senza contare l'ampio numero di features
-   facilmente selezionabili e customizzabili senza bisogno di essere esperti programmatori!
+    Questa versione di GDRCD corregge un importante numero di criticità riscontrate nella versione precedente e
+    stabilisce la basi per una prossima ottimizzazione del core principale delle funzioni che compongono il CMS,
+    sia in termini di <strong>qualità</strong>, che di <strong>stabilità</strong> e <strong>sicurezza</strong>.
+    Sono state implementate nuove features su iniziativa del Team di GDRCD o provenienti dalla communità di
+    <a href="https://gdr-online.com/" target="_blank">GdR-Online.com</a>, quest'ultime completamente riviste
+    per essere quanto più personalizzabili in base alle esigenze.
 </p>
 <p>
     I files della corrente versione, a fronte di tutte queste novità, <strong>non sono retrocompatibili</strong> nei
-   riguardi di MrFaber's GDRCD5 (se non con un ampio lavoro di rielaborazione) tuttavia il software è progettato per
-    <strong>rilevare il database di un installazione precedente ed aggiornarlo autonomamente</strong> al fine di
-   renderlo compatibile con la presente versione.
+    riguardi di <strong>GDRCD 5.4</strong> di <strong>breaker</strong> (se non con un ampio lavoro di rielaborazione)
+    tuttavia il software è progettato per <strong>rilevare il database di un installazione precedente ed aggiornarlo autonomamente</strong>
+    al fine di renderlo compatibile con la presente versione.
 </p>
 <p>
-    Troverai l'elenco delle nuove features e dei bug fix al <a href="http://www.gdr-online.com/readforum.asp?id=135483">topic di presentazione</a> sul portale GDR-Online.com
+    Puoi trovare tutte le informazioni sulle modifiche eseguite sulla repository ufficiale di <a href="https://github.com/GDRCD/GDRCD">GitHub</a>
+    a questo <a href="https://github.com/GDRCD/GDRCD/releases/tag/5.6">indirizzo</a>.
 </p>
 <p>
-    Prima di iniziare a realizzare il tuo sito, hai letto la <a href="licenza.txt">licenza d'uso</a> che accompagna il prodotto? Se non l'hai ancora fatto premurati gentilmente di leggerla.
+    Prima di iniziare a realizzare il tuo sito, hai letto la <a href="license.md">licenza d'uso</a> che accompagna il prodotto? Se non l'hai ancora fatto premurati gentilmente di leggerla.
 </p>
 <p>
-    Troverai le istruzioni su come utilizzare GDRCD 5.1 nel <a href="COME-FARE-LA-LAND---LEGGIMI!.txt">manuale</a> allegato.
+    Troverai le istruzioni su come utilizzare GDRCD 5.6 nel <a href="ISTRUZIONI.txt">manuale</a> allegato.
 </p>
 <img src="imgs/pelready.png" style="float:left;">
 <p><br /><strong>PEL Ready</strong></p>
 <p>
-    Il presente Software fornisce gli strumenti sul piano tecnico per aderire in semplicità al <a href="http://www.gdr-online.com/progetto_legalita.asp">progetto di educazione alla legalità</a> per i giochi di ruolo online al fine di sensibilizzare gli utilizzatori del CMS a tale iniziativa, nel rispetto e nella sicurezza dei possibili futuri giocatori.
+    Il presente Software fornisce gli strumenti sul piano tecnico per aderire in semplicità al <a href="http://www.gdr-online.com/progetto_legalita.asp">progetto di educazione alla legalità</a>
+    per i giochi di ruolo online al fine di sensibilizzare gli utilizzatori del CMS a tale iniziativa, nel rispetto e nella sicurezza dei possibili futuri giocatori.
 </p>
 <p>
-    Ti piace il progetto ? Lo hai trovato particolarmente utile ? Allora scopri come puoi <a href="http://www.gdr-online.com/readforum.asp?id=137092">contribuire al suo sviluppo</a> ;-)
+    Ti piace il progetto ? Lo hai trovato particolarmente utile ?
+    Allora scopri come puoi contribuire al suo sviluppo dando un occhiata al <a href="http://www.gdr-online.com/readforum.asp?id=137092">topic dedicato</a> su <a href="https://gdr-online.com/" target="_blank">GdR-Online.com</a>,
+    leggendo la <a href="https://github.com/GDRCD/GDRCD/wiki">Wiki su GitHub</a> o iscrivendoti al <a href="https://discord.gg/DJWaXPrQPQ">Server Discord Ufficiale</a>, dedicato al confronto, allo sviluppo e al mantimento di GDRCD.
+    <u>Ogni aiuto che viene dato è prezioso più di quel che sembra!</u>
 </p>
 
 <p>Buon diverimento!</p>
