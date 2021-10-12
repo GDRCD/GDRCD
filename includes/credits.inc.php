77fa0531269413b4f714ce06bc30a4ad6dfaff3d
Luca Pattocchio <pattocchioluca@gmail.com>
2021-10-12 11:56:25 +0200

    HOME [FIX] Aggiornati i credits

--- includes/credits.inc.php
+++ includes/credits.inc.php
@@ -1,5 +1,3 @@
 <?php
-$CREDITS = gdrcd_filter('out',
-    'Basato su "GDRCD '.$GLOBALS['PARAMETERS']['info']['GDRCD'].'". CMS Open Source sviluppato da Breaker e distribuito gratuitamente.');
-$LICENCE = '<a href="licenza.txt">' . gdrcd_filter('out', 'Licenza d\'uso e riproduzione') . '</a>.';
-?>
+$CREDITS = gdrcd_filter('out', 'Basato su "GDRCD '.$GLOBALS['PARAMETERS']['info']['GDRCD'].'". CMS Open Source sviluppato dal <a href="https://github.com/orgs/GDRCD/people" target="_blank">Team di GDRCD</a> e distribuito gratuitamente.');
+$LICENCE = '<a href="license.md" target="_blank">' . gdrcd_filter('out', 'Licenza d\'uso e riproduzione') . '</a>.';
