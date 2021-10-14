0a3390afc1905f291a446fd334fdc1efd26a52da
Luca Pattocchio <pattocchioluca@gmail.com>
2021-10-14 23:59:32 +0200

    HOME [FIX] Aggiornati gli stili dell'header

--- pages/homepage/index.inc.php
+++ pages/homepage/index.inc.php
@@ -21,23 +21,28 @@ $users = gdrcd_query("SELECT COUNT(nome) AS online FROM personaggio WHERE ora_en
     <div id="site_width">
 
         <div id="header">
-            <div class="login_form">
-                <form action="login.php" id="do_login" method="post"
+            <div class="titlecontent">
+                <h1><a href="index.php"><?php echo $MESSAGE['homepage']['main_content']['site_title']; ?></a></h1>
+                <div class="subtitle"><?php echo $MESSAGE['homepage']['main_content']['site_subtitle']; ?></div>
+            </div>
+            <div class="logincontent">
+                <div class="login_form">
+                    <form action="login.php" id="do_login" method="post"
                         <?php if ($PARAMETERS['mode']['popup_choise'] == 'ON') { echo ' onsubmit="check_login(); return false;"';} ?>
-                >
-                    <div>
-                        <span class="form_label"><label for="username"><?php echo $MESSAGE['homepage']['forms']['username']; ?></label></span>
-                        <input type="text" id="username" name="login1"/>
-                    </div>
-                    <div>
-                        <span class="form_label"><label for="password"><?php echo $MESSAGE['homepage']['forms']['password']; ?></label></span>
-                        <input type="password" id="password" name="pass1"/>
-                    </div>
-                    <?php if (!empty($PARAMETERS['themes']['available']) and count($PARAMETERS['themes']['available']) > 1): ?>
+                    >
                         <div>
-                            <span class="form_label"><label for="theme"><?= gdrcd_filter('out', $MESSAGE['homepage']['forms']['theme_choice']) ?></label></span>
-                            <select name="theme" id="theme">
-                                <?php
+                            <span class="form_label"><label for="username"><?php echo $MESSAGE['homepage']['forms']['username']; ?></label></span>
+                            <input type="text" id="username" name="login1"/>
+                        </div>
+                        <div>
+                            <span class="form_label"><label for="password"><?php echo $MESSAGE['homepage']['forms']['password']; ?></label></span>
+                            <input type="password" id="password" name="pass1"/>
+                        </div>
+                        <?php if (!empty($PARAMETERS['themes']['available']) and count($PARAMETERS['themes']['available']) > 1): ?>
+                            <div>
+                                <span class="form_label"><label for="theme"><?= gdrcd_filter('out', $MESSAGE['homepage']['forms']['theme_choice']) ?></label></span>
+                                <select name="theme" id="theme">
+                                    <?php
                                     foreach ($PARAMETERS['themes']['available'] as $k => $name) {
                                         echo '<option value="' . gdrcd_filter('out', $k) . '"';
                                         if ($k == $PARAMETERS['themes']['current_theme']) {
@@ -46,21 +51,20 @@ $users = gdrcd_query("SELECT COUNT(nome) AS online FROM personaggio WHERE ora_en
                                         echo '>' . gdrcd_filter('out', $name) . '</option>';
                                     }
                                     ?>
-                            </select>
-                        </div>
-                    <?php endif; ?>
-                    <?php if ($PARAMETERS['mode']['popup_choise'] == 'ON') { ?>
-                        <div>
-                        <span class="form_label"><label for="allow_popup"><?php echo $MESSAGE['homepage']['forms']['open_in_popup']; ?></label></span>
-                        <input type="checkbox" id="allow_popup"/>
-                        <input type="hidden" value="0" name="popup" id="popup">
-                        </div>
-                    <?php } ?>
-                    <input type="submit" value="<?php echo $MESSAGE['homepage']['forms']['login']; ?>"/>
-                </form>
+                                </select>
+                            </div>
+                        <?php endif; ?>
+                        <?php if ($PARAMETERS['mode']['popup_choise'] == 'ON') { ?>
+                            <div>
+                                <span class="form_label"><label for="allow_popup"><?php echo $MESSAGE['homepage']['forms']['open_in_popup']; ?></label></span>
+                                <input type="checkbox" id="allow_popup"/>
+                                <input type="hidden" value="0" name="popup" id="popup">
+                            </div>
+                        <?php } ?>
+                        <input type="submit" value="<?php echo $MESSAGE['homepage']['forms']['login']; ?>"/>
+                    </form>
+                </div>
             </div>
-            <h1><a href="index.php"><?php echo $MESSAGE['homepage']['main_content']['site_title']; ?></a></h1>
-            <div class="subtitle"><?php echo $MESSAGE['homepage']['main_content']['site_subtitle']; ?></div>
         </div>
 
         <div id="content">
