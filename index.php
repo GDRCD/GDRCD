006822273205e3d99375d0cf9fd0ad678bdb7742
Luca Pattocchio <pattocchioluca@gmail.com>
2021-10-14 23:34:34 +0200

    GDRCD [FEAT] Diviso il tema della Homepage da quello della Mainpage

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <?php
-$dont_check = true;
-$check_for_update = false;
 
-require_once 'config.inc.php';
+header('Content-Type:text/html; charset=UTF-8');
 
+//Includo i parametri, la configurazione, la lingua e le funzioni
+require_once('includes/required.php');
+
+//Eseguo la connessione al database
+$handleDBConnection = gdrcd_connect();
+
+/**
+ * Nel caso stessi utilizzando un sistema di protezione per il sito, prevedo il caricamento della pagina
+ * @author Breaker
+ */
 if ($PARAMETERS['settings']['protection'] == 'ON'){
     require 'protezione.php';
 }
-
-require('header.inc.php'); /*Header comune*/
 
 /*
  * Fix per installare il database la prima volta.
@@ -29,7 +35,30 @@ $page = ( !empty($_GET['page']) && $_GET['page'] != 'homepage' ) ? 'homepage__' 
  */
 $content = ( ! empty($_GET['content'])) ? gdrcd_filter('include', $_GET['content']) : 'home';
 
-// Includo la pagina
-gdrcd_load_modules($page, ['content' => $content]);
 
-require 'footer.inc.php';
+/**
+ * Avvio la costruzione ddei contenuti della pagina
+ * @author Kasa
+ */
+?>
+<!--Force IE6 into quirks mode with this comment tag-->
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <!-- IE9: mi stai ampiamente rompendo i maroni. -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="shortcut icon" href="favicon.png" type="image/png" />
+    <link rel="stylesheet" href="themes/homepage/<?=$PARAMETERS['themes']['homepage'];?>/homepage.css" type="text/css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" />
+    <title>
+        <?php echo $PARAMETERS['info']['site_name']; ?>
+    </title>
+</head>
+<body class="main_body">
+<?php
+
+    // Includo la pagina
+    gdrcd_load_modules($page, ['content' => $content]);
+
+    require 'footer.inc.php';
