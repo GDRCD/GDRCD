99eb34492ead6ca8aaaa5897c77fecbf71984bca
Jansna <gd7j91r3>
2021-10-24 18:25:58 +0200

    Pacchetto gestione quest [NEW]

--- config.inc.php
+++ config.inc.php
@@ -33,9 +33,9 @@ if( ! empty($_SESSION['login'])) {
 }
 
 /* PARAMETRI DI CONNESSIONE */
-$PARAMETERS['database']['username'] = 'provabea';            //nome utente del database
-$PARAMETERS['database']['password'] = '65NCyZVfYcRF';            //password del database
-$PARAMETERS['database']['database_name'] = 'my_provabea';    //nome del database
+$PARAMETERS['database']['username'] = 'gdrcd';            //nome utente del database
+$PARAMETERS['database']['password'] = 'gdrcd';            //password del database
+$PARAMETERS['database']['database_name'] = 'my_gdrcd';    //nome del database
 $PARAMETERS['database']['url'] = 'localhost';        //indirizzo ip del database
 
 
