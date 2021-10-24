989c47808f7842a5378c600920041d2664b6ae53
gianni10049 <38789905+gianni10049@users.noreply.github.com>
2021-10-24 19:53:35 +0200

    GDRCD [FIX] Ripristinato file originale del dev

--- pages/forum.inc.php
+++ pages/forum.inc.php
@@ -13,16 +13,16 @@
          */
         switch(gdrcd_filter_get($_POST['op'])) {
             case 'delete': //Cancellazione messaggio o topic
-                include('forum/delete.inc.php');
+                include ('forum/delete.inc.php');
                 break;
             case 'edit': //Modifica messaggio o topic
-                include('forum/edit.inc.php');
+                include ('forum/edit.inc.php');
                 break;
             case 'insert': //Inserimento messaggio o topic
-                include('forum/insert.inc.php');
+                include ('forum/insert.inc.php');
                 break;
             case 'readall': //Funzione segna tutto come letto
-                include('forum/readall.inc.php');
+                include ('forum/readall.inc.php');
                 break;
             default:
                 break;
@@ -32,23 +32,23 @@
          */
         switch(gdrcd_filter_get($_GET['op'])) {
             case 'composer': //Creazione nuovi messaggi e topic
-                include('forum/composer.inc.php');
+                include ('forum/composer.inc.php');
                 break;
             case 'delete_conf':
-                include('forum/delete_conf.inc.php');
+                include ('forum/delete_conf.inc.php');
                 break;
             case 'modifica': //Form modifica
-                include('forum/modifica.inc.php');
+                include ('forum/modifica.inc.php');
                 break;
             case 'read': //Visualizzazione topic
-                include('forum/read.inc.php');
+                include ('forum/read.inc.php');
                 break;
             case 'visit': //Visualizzazione dei topic
-                include('forum/visit.inc.php');
+                include ('forum/visit.inc.php');
                 break;
             case false: //Visualizzazione di base (Elenco forum)
             default:
-                include('forum/index.inc.php');
+                include ('forum/index.inc.php');
                 break;
         }
 
