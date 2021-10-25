7c56fa8983a84bc89afb9983574a3a0407df0a56
gianni10049 <38789905+gianni10049@users.noreply.github.com>
2021-10-25 19:43:42 +0200

    GDRCD [Routing] Creata classe base per l'estensione delle altre

--- classes/example.class.php
+++ classes/example.class.php
@@ -2,16 +2,5 @@
 
 class Example extends BaseClass{
 
-    /**
-     * Init vars PUBLIC STATIC
-     * @var Router $_instance ;
-     */
-    public static
-        $_instance;
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
 
 }
--- includes/base.class.php
+++ includes/base.class.php
@@ -1,6 +1,7 @@
 <?php
 
-class BaseClass{
+abstract class BaseClass
+{
 
     /**
      * Init vars PUBLIC STATIC
@@ -8,21 +9,32 @@ class BaseClass{
      */
     public static
         $_instance;
+    /**
+     * @var string $me
+     * @var int $perm
+     */
+    protected $me,
+        $permission;
 
-    public function __construct()
+    protected function __construct()
     {
+        $this->me = gdrcd_filter('out', $_SESSION['login']);
+        $this->permission = gdrcd_filter('num', $_SESSION['permessi']);
     }
 
     /**
      * @fn getInstance
      * @note Self Instance for static recall (BaseClass::__construct();)
-     * @return self
+     * @return mixed
      */
-    public static function getInstance():self
+    public static function getInstance()
     {
-        if (!(self::$_instance instanceof self)) {
-            self::$_instance = new self();
+        $className = get_called_class();
+
+        if (!($className::$_instance instanceof $className)) {
+            $className::$_instance = new $className();
         }
-        return self::$_instance;
+
+        return $className::$_instance;
     }
 }
