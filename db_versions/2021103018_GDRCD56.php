94fb7f667b38d9bcd0715379e4278309b8933edf
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2021-10-31 13:33:14 +0100

    GDRCD [DBMigrations] Implementata migration per GDRCD5.6

--- db_versions/2021103018_GDRCD56.php
+++ db_versions/2021103018_GDRCD56.php
@@ -12,7 +12,105 @@ class GDRCD56 extends DbMigration
      */
     public function up()
     {
-        // TODO: Implement up() method.
+        gdrcd_query("ALTER TABLE backmessaggi
+ALTER spedito SET default CURRENT_TIMESTAMP");
+    
+        gdrcd_query("ALTER TABLE backmessaggi
+ADD COLUMN tipo int(2) NOT NULL default '0'");
+    
+        gdrcd_query("ALTER TABLE backmessaggi
+ADD COLUMN oggetto text NULL DEFAULT NULL");
+        
+        gdrcd_query("CREATE TABLE IF NOT EXISTS blocco_esiti
+(
+    id     int(11)                         NOT NULL AUTO_INCREMENT,
+    titolo text CHARACTER SET utf8         NOT NULL,
+    data   datetime                        NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    autore varchar(255) CHARACTER SET utf8 NOT NULL,
+    pg     varchar(255) CHARACTER SET utf8 NOT NULL,
+    id_min int(11)                                  DEFAULT NULL,
+    master varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '0',
+    closed int(11)                         NOT NULL DEFAULT '0',
+    PRIMARY KEY (id)
+) ENGINE = MyISAM
+  DEFAULT CHARSET = utf8;");
+    
+        gdrcd_query("ALTER TABLE chat
+ALTER COLUMN ora SET DEFAULT CURRENT_TIMESTAMP");
+    
+        gdrcd_query("CREATE TABLE esiti (
+        id int(11) NOT NULL AUTO_INCREMENT,
+        sent int(11) NOT NULL DEFAULT '0',
+        titolo varchar(255) CHARACTER SET utf8 NOT NULL,
+        data datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        chat int(11) NOT NULL DEFAULT '0',
+         autore varchar(255) CHARACTER SET utf8 NOT NULL,
+        contenuto mediumtext CHARACTER SET utf8,
+        noteoff text CHARACTER SET utf8,
+        master varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+        pg varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+        id_blocco int(11) NOT NULL,
+        id_ab int(11) DEFAULT '0',
+        CD_1 text CHARACTER SET utf8,
+        CD_2 text CHARACTER SET utf8,
+        CD_3 text CHARACTER SET utf8,
+        CD_4 text CHARACTER SET utf8,
+        letto_master int(11) NOT NULL DEFAULT '0',
+        letto_pg int(11) NOT NULL DEFAULT '0',
+        dice_face int(4) NOT NULL DEFAULT '0',
+        dice_num int(4) NOT NULL DEFAULT '0',
+        dice_results varchar(1000) DEFAULT NULL,
+        PRIMARY KEY (id)
+        ) ENGINE=MyISAM DEFAULT CHARSET=utf8;");
+    
+        gdrcd_query("ALTER TABLE gilda
+ALTER COLUMN data_evento SET DEFAULT CURRENT_TIMESTAMP");
+    
+        gdrcd_query("ALTER TABLE messaggi
+ALTER COLUMN spedito SET DEFAULT CURRENT_TIMESTAMP");
+        
+        gdrcd_query("ALTER TABLE messaggi
+ADD COLUMN tipo int(2) NOT NULL default '0'");
+        
+        gdrcd_query("ALTER TABLE messaggi
+ADD COLUMN oggetto text NULL DEFAULT NULL");
+    
+        gdrcd_query("ALTER TABLE messaggioaraldo
+ALTER COLUMN data_messaggio SET DEFAULT CURRENT_TIMESTAMP");
+    
+        gdrcd_query("ALTER TABLE messaggioaraldo
+ALTER COLUMN data_ultimo_messaggio SET DEFAULT CURRENT_TIMESTAMP");
+    
+        gdrcd_query("ALTER TABLE oggetto
+ALTER COLUMN data_inserimento SET DEFAULT CURRENT_TIMESTAMP");
+    
+        gdrcd_query("ALTER TABLE personaggio
+CHANGE COLUMN ora_uscita ora_uscita datetime NOT NULL default '2009-07-01 00:00:00'");
+    
+        gdrcd_query("ALTER TABLE personaggio
+DROP COLUMN ultimo_messaggio");
+    
+        gdrcd_query("CREATE TABLE segnalazione_role (
+          id bigint(20) NOT NULL AUTO_INCREMENT,
+          stanza int(11) NOT NULL,
+          conclusa int(11) NOT NULL DEFAULT '0',
+          partecipanti text CHARACTER SET utf8,
+          mittente varchar(20) CHARACTER SET utf8 NOT NULL,
+          data_inizio datetime DEFAULT NULL,
+          data_fine datetime DEFAULT NULL,
+          tags text CHARACTER SET utf8,
+          quest text CHARACTER SET utf8,
+          PRIMARY KEY (id)
+        ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;");
+    
+        gdrcd_query("CREATE TABLE send_GM (
+          id int(11) NOT NULL AUTO_INCREMENT,
+          data datetime NOT NULL,
+          autore text CHARACTER SET utf8 NOT NULL,
+          role_reg int(11) NOT NULL,
+          note text CHARACTER SET utf8,
+          PRIMARY KEY (id)
+        ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;");
     }
     
     /**
@@ -20,6 +118,51 @@ class GDRCD56 extends DbMigration
      */
     public function down()
     {
-        // TODO: Implement down() method.
+        gdrcd_query("ALTER TABLE backmessaggi
+ALTER spedito SET default '0000-00-00 00:00:00'");
+    
+        gdrcd_query("ALTER TABLE backmessaggi
+DROP COLUMN tipo");
+    
+        gdrcd_query("ALTER TABLE backmessaggi
+DROP COLUMN oggetto");
+    
+        gdrcd_query("DROP TABLE blocco_esiti");
+    
+        gdrcd_query("ALTER TABLE chat
+ALTER COLUMN ora SET DEFAULT '0000-00-00 00:00:00'");
+    
+        gdrcd_query("DROP TABLE esiti");
+    
+        gdrcd_query("ALTER TABLE gilda
+ALTER COLUMN data_evento SET DEFAULT '0000-00-00 00:00:00'");
+    
+        gdrcd_query("ALTER TABLE messaggi
+ALTER COLUMN spedito SET DEFAULT '0000-00-00 00:00:00'");
+    
+        gdrcd_query("ALTER TABLE messaggi
+DROP COLUMN tipo");
+    
+        gdrcd_query("ALTER TABLE messaggi
+DROP COLUMN oggetto");
+    
+        gdrcd_query("ALTER TABLE messaggioaraldo
+ALTER COLUMN data_messaggio SET DEFAULT NULL");
+    
+        gdrcd_query("ALTER TABLE messaggioaraldo
+ALTER COLUMN data_ultimo_messaggio SET DEFAULT NULL");
+    
+        gdrcd_query("ALTER TABLE oggetto
+ALTER COLUMN data_inserimento SET DEFAULT '0000-00-00 00:00:00'");
+    
+        gdrcd_query("ALTER TABLE personaggio
+CHANGE COLUMN ora_uscita ora_uscita datetime default NULL");
+    
+        gdrcd_query("ALTER TABLE personaggio
+ADD COLUMN ultimo_messaggio bigint(20) NOT NULL default '0'");
+    
+        gdrcd_query("DROP TABLE segnalazione_role");
+    
+        gdrcd_query("DROP TABLE send_GM");
     }
 }
