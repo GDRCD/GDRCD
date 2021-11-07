2965a92af7ca22fc8c1132d00206897a49110a9d
gianni10049 <38789905+gianni10049@users.noreply.github.com>
2021-11-07 13:43:16 +0100

    GDRCD [Hotfix] Errore nei valori di default che creavano problemi ad inserimento

--- db_versions/2021103019_GDRCD5601.php
+++ db_versions/2021103019_GDRCD5601.php
@@ -10,12 +10,12 @@ class GDRCD5601 extends DbMigration
         gdrcd_query("CREATE TABLE IF NOT EXISTS diario (
             id int NOT NULL AUTO_INCREMENT,
             personaggio varchar(255) DEFAULT NULL,
-            data date NOT NULL,
-            data_inserimento datetime NOT NULL,
-            data_modifica datetime DEFAULT NULL,
-            visibile varchar(255) NOT NULL,
+            data date NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            data_inserimento datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            data_modifica datetime DEFAULT NULL DEFAULT CURRENT_TIMESTAMP,
+            visibile varchar(255) NOT NULL DEFAULT 1,
             titolo varchar(255) NOT NULL DEFAULT '',
-            testo text NOT NULL,
+            testo text DEFAULT NULL,
             PRIMARY KEY (id)
         ) ENGINE=MyISAM DEFAULT CHARSET=utf8;");
 
