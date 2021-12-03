ff8a1e6d0b11ec38ae9b95c96464e76155c3a024
Luca Pattocchio <pattocchioluca@gmail.com>
2021-12-03 12:43:18 +0100

    GDRCD 5.6.0.1 - Miglioramento audio in chat

--- includes/required.php
+++ includes/required.php
@@ -13,6 +13,9 @@ require_once dirname(__FILE__).'/DbMigration.class.php';
 require_once(dirname(__FILE__) . '/../vocabulary/' . $PARAMETERS['languages']['set'] . '.vocabulary.php');
 require_once(dirname(__FILE__) . '/functions.inc.php');
 
+/** Gestione dei Suoni */
+require_once(dirname(__FILE__) . '/AudioController.php');
+
 if(!empty($_SESSION['theme']) and array_key_exists($_SESSION['theme'], $PARAMETERS['themes']['available'])){
     $PARAMETERS['themes']['current_theme'] = $_SESSION['theme'];
 }
