2d78412d2a3aef077e1bdafec941b2db57ac94e7
annamereu <74818550+annamereu@users.noreply.github.com>
2022-01-25 01:23:39 +0100

    GDRCD 6.0 - Gathering impostazioni delle classi e delle costanti.

---
+++ classes/Controller/Gathering/GatheringItem.class.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @class GatheringItem
+ * @note Classe per la gestione degli oggetti trovabili
+ * @required PHP 7.1+
+ */
+class GatheringItem extends Gathering
+{
+
+}
---
+++ pages/gestione_gathering.inc.php
@@ -0,0 +1,2 @@
+
+<?php require(__DIR__ . '/gestione/gathering/gathering_index.php'); ?>
