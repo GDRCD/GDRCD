0e47f5b6f61d3d8cca410b29569ae101e7b27839
Luca Pattocchio <pattocchioluca@gmail.com>
2022-06-01 13:13:39 +0200

    GDRCD 5.6 - Routing Fix
    - Migliorata generazione contenuto pagine Main + Modulo Forum
    
    (cherry picked from commit 8360b3010ac79b9296327a888a875ef60a9a51f3)

--- layouts/left-top_frames.php
+++ layouts/left-top_frames.php
@@ -89,7 +89,7 @@ if (isset($_GET['css']))
                     {
                         echo '<div class="' . $box['class'] . '">';
 
-                        gdrcd_load_modules('pages/' . $box['page'] . '.inc.php', $box);
+                        gdrcd_load_modules($box['page'], $box);
 
                         echo '</div>';
                     }
@@ -119,7 +119,7 @@ if (isset($_GET['css']))
                     {
                         echo '<div class="' . $box['class'] . '">';
 
-                        gdrcd_load_modules('pages/' . $box['page'] . '.inc.php', $box);
+                        gdrcd_load_modules($box['page'], $box);
 
                         echo '</div>';
 
@@ -138,7 +138,7 @@ if (isset($_GET['css']))
 
     <div id="maincontent">
         <div class="output">
-            <?php gdrcd_load_modules('pages/' . $strInnerPage); ?>
+            <?php gdrcd_load_modules($strInnerPage); ?>
         </div>
     </div>
 
