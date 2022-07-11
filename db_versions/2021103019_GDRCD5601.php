94fe721612f0bf8fcfdc4e450a69a86d8e7293f4
annamereu <74818550+annamereu@users.noreply.github.com>
2022-07-11 13:52:08 +0200

    Update 2021103019_GDRCD5601.php

--- db_versions/2021103019_GDRCD5601.php
+++ db_versions/2021103019_GDRCD5601.php
@@ -20,6 +20,21 @@ class GDRCD5601 extends DbMigration
         ) ENGINE=MyISAM DEFAULT CHARSET=utf8;");
 
         gdrcd_query("ALTER TABLE personaggio ADD storia text DEFAULT NULL;");
+
+        gdrcd_query("CREATE TABLE IF NOT EXISTS eventi (
+            id INT(11) NOT NULL AUTO_INCREMENT,
+            title VARCHAR(255)DEFAULT NULL,
+            start DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            end DATETIME NULL DEFAULT NULL DEFAULT CURRENT_TIMESTAMP,
+            titolo VARCHAR(250) DEFAULT NULL,
+            descrizione TEXT DEFAULT NULL,
+            backgroundColor VARCHAR(50) DEFAULT NULL,
+            borderColor VARCHAR(50) DEFAULT NULL,
+            textColor VARCHAR(50) DEFAULT NULL,
+            PRIMARY KEY (id) USING BTREE
+        )ENGINE=MyISAM DEFAULT CHARSET=utf8;");
+
+
     }
 
     /**
