ed3151f874ba6997f26a7178d659171f6ca262d7
Salvatore <s.rotondo90@gmail.com>
2022-08-20 01:06:45 +0200

    Realizzato un metodo per distruggere propriamente una sessione attiva ed un veloce refactoring della pagina di logout per utilizzarlo come si deve

--- core/classes/Libraries/Session.class.php
+++ core/classes/Libraries/Session.class.php
@@ -19,6 +19,31 @@ class Session
     }
 
     /**
+     * @fn destroy
+     * @note distrugge la sessione corrente
+     * @return void
+     */
+    public static function destroy(): void
+    {
+        $_SESSION = [];
+
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                '',
+                time() - 86400,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
+        }
+
+        session_destroy();
+    }
+
+    /**
      * @fn reset
      * @note Ripristina nei dati in sessione i valori che erano presenti all'avvio
      * @return void
