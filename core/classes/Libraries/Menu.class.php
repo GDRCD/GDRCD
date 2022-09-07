1c75b9d8203331d896dd947a729cbcda382fbda5
gianni10049 <giovanni.pane10049@gmail.com>
2022-09-07 11:42:35 +0200

    GDRCD 6 - [MENU] - Refactor menu with template

--- core/classes/Libraries/Menu.class.php
+++ core/classes/Libraries/Menu.class.php
@@ -16,18 +16,24 @@ class Menu extends BaseClass
 
         $categories = [];
 
+        // Genero categorie
         foreach ( $menu_category as $section ) {
-            $links = DB::queryStmt("SELECT * FROM menu WHERE section=:section ORDER BY name", ['section' => Filters::out($section['section'])]);
 
+            // Creo array base da aggiungere
             $data = [
                 "name" => Filters::out($section['section']),
             ];
 
+            // Estraggo i link
+            $links = DB::queryStmt("SELECT * FROM menu WHERE section=:section ORDER BY name", ['section' => Filters::out($section['section'])]);
+
+            // Creo i link
             foreach ( $links as $link ) {
                 $name = Filters::out($link['name']);
                 $page = Filters::out($link['page']);
                 $permission = Filters::out($link['permission']);
 
+                // Se ho i permessi per vedere il link, lo aggiungo
                 if ( empty($permission) || Permissions::permission($permission) ) {
                     $data['links'][] = [
                         "page" => $page,
@@ -36,10 +42,11 @@ class Menu extends BaseClass
                 }
             }
 
+            // Aggiungo l'array formato a quello generale
             $categories[] = $data;
-
         }
 
+        // Renderizzo il menu'
         return Template::getInstance()->startTemplate()->render('menu', ['categories' => $categories]);
     }
 }
