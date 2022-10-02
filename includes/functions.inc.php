ceff5efad0fa9ede1651a327254e806634718e28
Kasui92 <pattocchioluca@gmail.com>
2022-10-02 21:18:30 +0200

    GDRCD [FIX] Sostituita funzione deprecata strftime;

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -541,7 +541,7 @@ function gdrcd_check_time($time)
 
     if ($time_hours == date('H')) {
         return date('i') - $time_minutes;
-    } elseif ($time_hours == (date('H') - 1) || $time_hours == (strftime('H') + 11)) {
+    } elseif ($time_hours == (date('H') - 1) || $time_hours == (date('H') + 11)) {
         return date('i') - $time_minutes + 60;
     }
 
