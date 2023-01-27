7cad1bc5baea8f57f2bfa2fe6556e071f6fd9907
Kasui92 <pattocchioluca@gmail.com>
2023-01-27 22:02:47 +0100

    GESTIONE [FIX] Corretta determinazione posizione mappa in caso di creazione nuova mappa in Gestione Mappe

--- pages/gestione_mappe.inc.php
+++ pages/gestione_mappe.inc.php
@@ -100,7 +100,7 @@
                             <?php echo gdrcd_filter('out', $MESSAGE['interface']['administration']['maps']['position']); ?>
                         </div>
                         <div class='form_field'>
-                            <input name="posizione" value="<?php echo 0 + gdrcd_filter('out', $loaded_record['posizione']); ?>" />
+                            <input name="posizione" value="<?php echo 0 + gdrcd_filter('out', isset($loaded_record['posizione']) ? $loaded_record['posizione'] : 0); ?>" />
                         </div>
                         <div class='form_info'>
                             <?php echo gdrcd_filter('out', $MESSAGE['interface']['administration']['maps']['position_info']); ?>
