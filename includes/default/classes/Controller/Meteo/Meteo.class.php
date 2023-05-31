776263f7c467693adac7123bf8cc06a30c80b991
gianni10049 <giovanni.pane10049@gmail.com>
2023-05-31 12:52:00 +0200

    GDRCD 6 - [LOGIN]  - Fix

--- includes/default/classes/Controller/Meteo/Meteo.class.php
+++ includes/default/classes/Controller/Meteo/Meteo.class.php
@@ -103,9 +103,13 @@ class Meteo extends BaseClass
      */
     protected function weatherNeedRefresh(): bool
     {
-        $date = date('Y-m-d H:i:s',strtotime($this->weather_last_date));
-        return empty($this->weather_last_date) ||
-            (CarbonWrapper::DatesDifferenceHours($date, CarbonWrapper::getNow()) > $this->weather_update_range);
+        if($this->weather_last_date) {
+            $date = date('Y-m-d H:i:s', strtotime($this->weather_last_date));
+            return empty($this->weather_last_date) ||
+                (CarbonWrapper::DatesDifferenceHours($date, CarbonWrapper::getNow()) > $this->weather_update_range);
+        } else {
+            return true;
+        }
     }
 
     /**
