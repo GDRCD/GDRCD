d37a5247b4c6d7735b28275ac69ef6e2cc754eb9
anneth <74818550+annamereu@users.noreply.github.com>
2024-05-06 16:38:46 +0200

    sistemata la parte di richiamo delle funzioni anche in lettura chat

--- includes/function_chat.inc.php
+++ includes/function_chat.inc.php
@@ -2,24 +2,19 @@
 /**
  * Funzioni di core relative alla chat
  */
-function inviaParlato($testo, $tag){
-    settaTag($tag);
-    settaTipo('P');
-    gdrcd_query("INSERT INTO chat ( stanza, imgs, mittente, destinatario, ora, tipo, testo) VALUES (".$_SESSION['luogo'].", '".$_SESSION['sesso'].";".$_SESSION['img_razza']."', '".$_SESSION['login']."', '".gdrcd_capital_letter(gdrcd_filter('in', $tag))."', NOW(), 'P', '{$testo}')");
-}
-function inviaAzione($testo, $tag){
-    settaTag($tag);
-    settaTipo('A');
-    gdrcd_query("INSERT INTO chat ( stanza, imgs, mittente, destinatario, ora, tipo, testo) VALUES (".$_SESSION['luogo'].", '".$_SESSION['sesso'].";".$_SESSION['img_razza']."', '".$_SESSION['login']."', '".gdrcd_capital_letter(gdrcd_filter('in', $tag))."', NOW(), 'A', '{$testo}')");
-}
 
+
+/**
+ * Recupera il nome della chat
+ */
 function chat_name($id)
 {
     $chat_name = gdrcd_query("SELECT nome FROM mappa WHERE id = {$id} ");
-
     return $chat_name['nome'];
 }
-
+/**
+ * Controllo se si tratta di una chat pubblica o privata
+ */
 function controlloChat($id)
 {
     $query="SELECT * FROM mappa WHERE id = '{$id}'";
@@ -39,6 +34,78 @@ function controlloChat($id)
         return true;
     }
 }
+/**
+ * Settaggio dei SESSION per tag e tipo
+ */
+function settaTag($tag)
+{
+    $_SESSION['tag'] = $tag;
+}
+
+function settaTipo($tipo)
+{
+    $_SESSION['tipo_azione']=$tipo;
+}
+
+/**
+ * invio e lettura  Azione e parlato
+  */
+function inviaAzione($testo, $tag){
+    settaTag($tag);
+    settaTipo('A');
+    gdrcd_query("INSERT INTO chat ( stanza, imgs, mittente, destinatario, ora, tipo, testo) VALUES (".$_SESSION['luogo'].", '".$_SESSION['sesso'].";".$_SESSION['img_razza']."', '".$_SESSION['login']."', '".gdrcd_capital_letter(gdrcd_filter('in', $tag))."', NOW(), 'A', '{$testo}')");
+}
+function inviaParlato($testo, $tag){
+    settaTag($tag);
+    settaTipo('P');
+    gdrcd_query("INSERT INTO chat ( stanza, imgs, mittente, destinatario, ora, tipo, testo) VALUES (".$_SESSION['luogo'].", '".$_SESSION['sesso'].";".$_SESSION['img_razza']."', '".$_SESSION['login']."', '".gdrcd_capital_letter(gdrcd_filter('in', $tag))."', NOW(), 'P', '{$testo}')");
+}
+
+
+function Azione($azione)
+{
+    $add_chat="";
+    $PARAMETERS = $GLOBALS['PARAMETERS'];
+    if($PARAMETERS['mode']['chaticons'] == 'ON') {
+        $icone_chat = explode(";", gdrcd_filter('out', $azione['imgs']));
+        $add_icon = '<span class="chat_icons"> <img class="presenti_ico" src="themes/'.$PARAMETERS['themes']['current_theme'].'/imgs/races/'.$icone_chat[1].'"><img class="presenti_ico" src="imgs/icons/testamini'.$icone_chat[0].'.png"> </span>';
+    }
+    $pg=gdrcd_query("SELECT url_img_chat FROM personaggio WHERE nome = '".gdrcd_filter("in", "{$azione['mittente']}")."' ");
+    if($PARAMETERS['mode']['chat_avatar'] == 'ON' && ! empty($pg['url_img_chat'])) {
+        $chat_avatar = '<img src="'.$pg['url_img_chat'].'" class="chat_avatar" style="width:'.$PARAMETERS['settings']['chat_avatar']['width'].'px; height:'.$PARAMETERS['settings']['chat_avatar']['height'].'px;" />';
+
+        // Se è stato impostato il link sull'avatar di chat, avvio la costruzione
+        if(isset($PARAMETERS['settings']['chat_avatar']['link']['mode']) and ($PARAMETERS['settings']['chat_avatar']['link']['mode']  == 'ON')) {
+            $chat_avatar_url = ( isset($PARAMETERS['settings']['chat_avatar']['link']['popup']) and ($PARAMETERS['settings']['chat_avatar']['link']['popup'] == 'ON') )
+                ? "javascript:modalWindow('scheda', 'Scheda di ". $azione['mittente'] ."', 'popup.php?page=scheda&pg=". $azione['mittente'] ."');"
+                : "main.php?page=scheda&pg=".$azione['mittente'];
+
+            // Inserisco l'avatar di chat cliccabile
+            $add_chat .= '<a href="'.$chat_avatar_url.'">'.$chat_avatar.'</a>';
+        }
+        // Altrimenti mostro solo l'avatar di chat
+        else {
+            $add_chat .= $chat_avatar;
+        }
+    }
+
+    $add_chat .= '<span class="chat_time">'.gdrcd_format_time($azione['ora']).'</span>';
+    if($PARAMETERS['mode']['chaticons'] == 'ON') {
+        $add_chat .= $add_icon;
+    }
+    $add_chat .= '<span class="chat_name"><a href="#" onclick="Javascript: document.getElementById(\'tag\').value=\''.$azione['mittente'].'\'; document.getElementById(\'tipo\')[2].selected = \'1\'; document.getElementById(\'message\').focus();">'.$azione['mittente'].'</a>';
+    if(empty ($row['destinatario']) === false) {
+        $add_chat .= '<span class="chat_tag"> ['.gdrcd_filter('out', $azione['destinatario']).']</span>';
+    }
+    $add_chat .=  ($azione['tipo'] === 'P') ? ': </span> ' : '</span> ';
+    $add_chat .= '<span class="chat_msg">'.gdrcd_chatme($_SESSION['login'], gdrcd_chatcolor(gdrcd_filter('out', $azione['testo']))).'</span>';
+    $add_chat .= '<br style="clear:both;" />';
+    return $add_chat;
+}
+
+/**
+ * Sussurri
+ */
 function inviaSussurro($testo, $tag)
 {
     $tag=ucfirst($tag);
@@ -59,28 +126,61 @@ function inviaSussurro($testo, $tag)
     settaTag('');
     settaTipo('');
 }
+
+function Sussurri($azione)
+{
+    $add_chat="";
+    //sussurri
+    if($_SESSION['login'] == $azione['destinatario']) {
+        $add_chat .= '<span class="chat_name">'.$azione['mittente'].' '.$MESSAGE['chat']['whisper']['by'].': </span> ';
+        $add_chat .= '<span class="chat_msg">'.gdrcd_filter('out', $azione['testo']).'</span>';
+    } elseif($_SESSION['login'] == $azione['mittente']) {
+        $add_chat .= '<span class="chat_msg">'.$MESSAGE['chat']['whisper']['to'].' '.gdrcd_filter('out', $azione['destinatario']).': </span>';
+        $add_chat .= '<span class="chat_msg">'.gdrcd_filter('out', $azione['testo']).'</span>';
+    } elseif(($_SESSION['permessi'] >= MODERATOR) && ($PARAMETERS['mode']['spyprivaterooms'] == 'ON')) {
+        $add_chat .= '<span class="chat_msg">'.$azione['mittente'].' '.$MESSAGE['chat']['whisper']['from_to'].' '.gdrcd_filter('out', $azione['destinatario']).' </span>';
+        $add_chat .= '<span class="chat_msg">'.gdrcd_filter('out', $azione['testo']).'</span>';
+    }
+    $add_chat .= '<br style="clear:both;" />';
+    return $add_chat;
+
+}
+
+/**
+ * invio / lettura Master
+ */
 function inviaMaster($testo){
 
     settaTipo('M');
     gdrcd_query("INSERT INTO chat ( stanza, imgs, mittente, destinatario, ora, tipo, testo) VALUES (".$_SESSION['luogo'].", '".$_SESSION['sesso'].";".$_SESSION['img_razza']."', '".$_SESSION['login']."', '', NOW(), 'M', '{$testo}')");
 }
 
-
+function Master($azione)
+{
+//Master
+    $add_chat= '<div class="chat_row_'.$azione['tipo'].'">';
+    $add_chat.= '<div class="ora_ms">'.gdrcd_format_time($azione['ora']).' Master Screen</div>';
+    $exp = explode('<br>',$azione['testo']);
+    $add_chat.= '<span class="chat_master">'.gdrcd_chatme($_SESSION['login'],  (gdrcd_bbcoder(gdrcd_filter('out',$exp[0])))).'</span><br><br>'.$exp[1];
+    $add_chat.= '</div>';
+    $add_chat .= '<br style="clear:both;" />';
+    return $add_chat;
+}
+/**
+ * invio / lettura PNG
+ */
 function inviaPNG($testo, $tag){
 
     settaTipo('N');
     gdrcd_query("INSERT INTO chat ( stanza, imgs, mittente, destinatario, ora, tipo, testo) VALUES (".$_SESSION['luogo'].", '".$_SESSION['sesso'].";".$_SESSION['img_razza']."', '".$_SESSION['login']."', '{$tag}', NOW(), 'N', '{$testo}')");
 }
-
-
-    function settaTag($tag)
-    {
-        $_SESSION['tag'] = $tag;
-    }
-
-    function settaTipo($tipo)
-    {
-        $_SESSION['tipo_azione']=$tipo;
-    }
-
+function PNG($azione)
+{
+    $add_chat= '<div class="chat_row_'.$azione['tipo'].'">';
+    $add_chat.= '<span class="chat_time">'.gdrcd_format_time($azione['ora']).'</span>';
+    $add_chat.= '<span class="chat_name">'.$azione['destinatario'].'</span> ';
+    $add_chat.= '<span class="chat_msg">'.gdrcd_chatcolor(gdrcd_filter('out',$azione['testo'])).'</span>';
+    $add_chat.= '</div>';
+    return $add_chat;
+}
 
--- pages/chat/chat_azioni.php
+++ pages/chat/chat_azioni.php
@@ -1,14 +1,10 @@
 <div class="chat_azioni">
-
     <?php
-
     $elenco_azioni=gdrcd_query("SELECT chat.id, chat.imgs, chat.mittente, chat.destinatario, chat.tipo, chat.ora, chat.testo,   mappa.ora_prenotazione
 						FROM chat
 						INNER JOIN mappa ON mappa.id = chat.stanza
 						LEFT JOIN personaggio ON personaggio.nome = chat.mittente
 						WHERE  stanza = {$_REQUEST['dir']} AND chat.ora > IFNULL(mappa.ora_prenotazione, '0000-00-00 00:00:00') AND DATE_SUB(NOW(), INTERVAL 4 HOUR) < ora ORDER BY id ASC ", "result");
-
-
     foreach ($elenco_azioni as $azione) {
         $tipo=$azione['tipo'];
         $add_chat="";
@@ -16,85 +12,25 @@
             case 'P':
             case 'A':
                 //azione
-
-                if($PARAMETERS['mode']['chaticons'] == 'ON') {
-                    $icone_chat = explode(";", gdrcd_filter('out', $azione['imgs']));
-                    $add_icon = '<span class="chat_icons"> <img class="presenti_ico" src="themes/'.$PARAMETERS['themes']['current_theme'].'/imgs/races/'.$icone_chat[1].'"><img class="presenti_ico" src="imgs/icons/testamini'.$icone_chat[0].'.png"> </span>';
-                }
-                $pg=gdrcd_query("SELECT url_img_chat FROM personaggio WHERE nome = '".gdrcd_filter("in", "{$azione['mittente']}")."' ");
-                if($PARAMETERS['mode']['chat_avatar'] == 'ON' && ! empty($pg['url_img_chat'])) {
-                    $chat_avatar = '<img src="'.$pg['url_img_chat'].'" class="chat_avatar" style="width:'.$PARAMETERS['settings']['chat_avatar']['width'].'px; height:'.$PARAMETERS['settings']['chat_avatar']['height'].'px;" />';
-
-                    // Se è stato impostato il link sull'avatar di chat, avvio la costruzione
-                    if(isset($PARAMETERS['settings']['chat_avatar']['link']['mode']) and ($PARAMETERS['settings']['chat_avatar']['link']['mode']  == 'ON')) {
-                        $chat_avatar_url = ( isset($PARAMETERS['settings']['chat_avatar']['link']['popup']) and ($PARAMETERS['settings']['chat_avatar']['link']['popup'] == 'ON') )
-                            ? "javascript:modalWindow('scheda', 'Scheda di ". $azione['mittente'] ."', 'popup.php?page=scheda&pg=". $azione['mittente'] ."');"
-                            : "main.php?page=scheda&pg=".$azione['mittente'];
-
-                        // Inserisco l'avatar di chat cliccabile
-                        $add_chat .= '<a href="'.$chat_avatar_url.'">'.$chat_avatar.'</a>';
-                    }
-                    // Altrimenti mostro solo l'avatar di chat
-                    else {
-                        $add_chat .= $chat_avatar;
-                    }
-                }
-
-                $add_chat .= '<span class="chat_time">'.gdrcd_format_time($azione['ora']).'</span>';
-                if($PARAMETERS['mode']['chaticons'] == 'ON') {
-                    $add_chat .= $add_icon;
-                }
-                $add_chat .= '<span class="chat_name"><a href="#" onclick="Javascript: document.getElementById(\'tag\').value=\''.$azione['mittente'].'\'; document.getElementById(\'tipo\')[2].selected = \'1\'; document.getElementById(\'message\').focus();">'.$azione['mittente'].'</a>';
-                if(empty ($row['destinatario']) === false) {
-                    $add_chat .= '<span class="chat_tag"> ['.gdrcd_filter('out', $azione['destinatario']).']</span>';
-                }
-                $add_chat .=  ($azione['tipo'] === 'P') ? ': </span> ' : '</span> ';
-                $add_chat .= '<span class="chat_msg">'.gdrcd_chatme($_SESSION['login'], gdrcd_chatcolor(gdrcd_filter('out', $azione['testo']))).'</span>';
-                /**    * Fix problema visualizzazione spazi vuoti con i sussurri
-                 * @author eLDiabolo
-                 */
-
+                echo Azione($azione);
                 break;
 
             case 'S':
-                //sussurri
-                if($_SESSION['login'] == $azione['destinatario']) {
-                    $add_chat .= '<span class="chat_name">'.$azione['mittente'].' '.$MESSAGE['chat']['whisper']['by'].': </span> ';
-                    $add_chat .= '<span class="chat_msg">'.gdrcd_filter('out', $azione['testo']).'</span>';
-                } elseif($_SESSION['login'] == $azione['mittente']) {
-                    $add_chat .= '<span class="chat_msg">'.$MESSAGE['chat']['whisper']['to'].' '.gdrcd_filter('out', $azione['destinatario']).': </span>';
-                    $add_chat .= '<span class="chat_msg">'.gdrcd_filter('out', $azione['testo']).'</span>';
-                } elseif(($_SESSION['permessi'] >= MODERATOR) && ($PARAMETERS['mode']['spyprivaterooms'] == 'ON')) {
-                    $add_chat .= '<span class="chat_msg">'.$azione['mittente'].' '.$MESSAGE['chat']['whisper']['from_to'].' '.gdrcd_filter('out', $azione['destinatario']).' </span>';
-                    $add_chat .= '<span class="chat_msg">'.gdrcd_filter('out', $azione['testo']).'</span>';
-                }
-
+               echo Sussurri($azione);
                 break;
             case 'C':
                 //visualizzazione lancio stat
-
-
                 break;
 
             case 'M':
-                //Master
-                $add_chat= '<div class="chat_row_'.$azione['tipo'].'">';
-                $add_chat.= '<div class="ora_ms">'.gdrcd_format_time($azione['ora']).' Master Screen</div>';
-                $exp = explode('<br>',$azione['testo']);
-                $add_chat.= '<span class="chat_master">'.gdrcd_chatme($_SESSION['login'],  (gdrcd_bbcoder(gdrcd_filter('out',$exp[0])))).'</span><br><br>'.$exp[1];
-                $add_chat.= '</div>';
+                echo Master($azione);
                 break;
             case 'N':
                 //PNG
-                $add_chat= '<div class="chat_row_'.$azione['tipo'].'">';
-                $add_chat.= '<span class="chat_time">'.gdrcd_format_time($azione['ora']).'</span>';
-                $add_chat.= '<span class="chat_name">'.$azione['destinatario'].'</span> ';
-                $add_chat.= '<span class="chat_msg">'.gdrcd_chatcolor(gdrcd_filter('out',$azione['testo'])).'</span>';
-                $add_chat.= '</div>';
+               echo PNG($azione);
                 break;
-
         }
-        $add_chat .= '<br style="clear:both;" />';
+
         echo $add_chat;
 
     }
@@ -107,7 +43,7 @@
                         FROM chat
 						INNER JOIN mappa ON mappa.id = chat.stanza
 						LEFT JOIN personaggio ON personaggio.nome = chat.mittente
-where stanza = '{$chat_id}'	   AND chat.ora > IFNULL(mappa.ora_prenotazione, '0000-00-00 00:00:00') AND DATE_SUB(NOW(), INTERVAL 24 HOUR) < ora");
+                where stanza = '{$chat_id}'	   AND chat.ora > IFNULL(mappa.ora_prenotazione, '0000-00-00 00:00:00') AND DATE_SUB(NOW(), INTERVAL 24 HOUR) < ora");
 
     ?>
 
