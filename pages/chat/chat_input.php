af1a3cb539266690f6fdaf665c0fc718d9926372
blancks <s.rotondo90@gmail.com>
2025-07-23 00:57:36 +0200

    Refactor della pagina responsabile per il form della chat. Migliorata la separazione tra parte logica e presentazione e la leggibilità generale del file

--- pages/chat/chat_input.php
+++ pages/chat/chat_input.php
@@ -3,162 +3,267 @@
     // Recupero le informazioni sulla chat corrente
     $chat_info ??= gdrcd_chat_info($_SESSION['luogo']);
 
+    // Diamo un nome a determinati controlli per semplificare la leggibilità di eventuali condizioni
+    $skillsystem_attivo = $PARAMETERS['mode']['skillsystem'] == 'ON';
+    $dadi_attivi = $PARAMETERS['mode']['dices'] == 'ON';
+    $login_gamemaster = $_SESSION['permessi'] >= GAMEMASTER;
+    $chat_privata = $chat_info['privata'] == 1;
+    $login_proprietario_chat = $chat_info['proprietario'] == $_SESSION['login'];
+    $gilda_proprietaria_chat = is_numeric($chat_info['proprietario'])
+        && str_contains($_SESSION['gilda'], (string)$chat_info['proprietario']);
+
+    // Elenco abilità del personaggio connesso
+    $skills = $skillsystem_attivo
+        ? gdrcd_chat_player_skills($_SESSION['login'])
+        : null;
+
+    // Elenco caratteristiche
+    $stats = $skillsystem_attivo
+        ? gdrcd_chat_player_stats()
+        : null;
+
+    // Elenco dadi disponibili
+    $dice = $dadi_attivi
+        ? gdrcd_chat_dice_list()
+        : null;
+
+    // Elenco oggetti equipaggiati del personaggio connesso
+    $items = $skillsystem_attivo
+        ? gdrcd_chat_player_items($_SESSION['login'])
+        : null;
+
 ?>
 <div class="chat_bottom">
-<form action="pages/chat/ajax.php?op=chat_write" method="post" id="azioneForm">
 
-        <div class="chat_text chat_row" >
+    <!-- Form per invio messaggi in chat -->
+    <form action="pages/chat/ajax.php?op=chat_write" method="post" id="azioneForm">
+        <div class="chat_text chat_row">
 
+            <!-- Tendina selezione tipo di azione -->
             <div class="input_container small">
                 <select name="type" id="type">
-                    <option value="P"><?php echo gdrcd_filter('out', $MESSAGE['chat']['type'][0]);//parlato?></option>
-                    <option value="A"><?php echo gdrcd_filter('out', $MESSAGE['chat']['type'][1]);//azione?></option>
-                    <option value="S"><?php echo gdrcd_filter('out', $MESSAGE['chat']['type'][4]);//sussurro?></option>
-                    <?php if($_SESSION['permessi'] >= GAMEMASTER) { ?>
-                        <option value="M"><?php echo gdrcd_filter('out', $MESSAGE['chat']['type'][2]);//master?></option>
-                        <option value="N"><?php echo gdrcd_filter('out', $MESSAGE['chat']['type'][3]);//png?></option>
+                    <option value="">Default</option>
+
+                    <!-- Parlato -->
+                    <option value="<?php echo gdrcd_filter('out', GDRCD_CHAT_MESSAGE_TYPE); ?>">
+                        <?php echo gdrcd_filter('out', $MESSAGE['chat']['type'][GDRCD_CHAT_MESSAGE_TYPE]); ?>
+                    </option>
+
+                    <!-- Azione -->
+                    <option value="<?php echo gdrcd_filter('out', GDRCD_CHAT_ACTION_TYPE); ?>">
+                        <?php echo gdrcd_filter('out', $MESSAGE['chat']['type'][GDRCD_CHAT_ACTION_TYPE]); ?>
+                    </option>
+
+                    <!-- Sussurra -->
+                    <option value="<?php echo gdrcd_filter('out', GDRCD_CHAT_WHISPER_TYPE); ?>">
+                        <?php echo gdrcd_filter('out', $MESSAGE['chat']['type'][GDRCD_CHAT_WHISPER_TYPE]); ?>
+                    </option>
+
+                    <?php if($login_gamemaster) { ?>
+
+                        <!-- Master -->
+                        <option value="<?php echo gdrcd_filter('out', GDRCD_CHAT_MASTER_TYPE); ?>">
+                            <?php echo gdrcd_filter('out', $MESSAGE['chat']['type'][GDRCD_CHAT_MASTER_TYPE]); ?>
+                        </option>
+
+                        <!-- PNG -->
+                        <option value="<?php echo gdrcd_filter('out', GDRCD_CHAT_PNG_TYPE); ?>">
+                            <?php echo gdrcd_filter('out', $MESSAGE['chat']['type'][GDRCD_CHAT_PNG_TYPE]); ?>
+                        </option>
+
+                        <!-- Immagine -->
+                        <option value="<?php echo gdrcd_filter('out', GDRCD_CHAT_IMAGE_TYPE); ?>">
+                            <?php echo gdrcd_filter('out', $MESSAGE['chat']['type'][GDRCD_CHAT_IMAGE_TYPE]); ?>
+                        </option>
+
                     <?php } ?>
-                    <?php if(($chat_info['privata'] == 1) && (($chat_info['proprietario'] == $_SESSION['login']) || ((is_numeric($chat_info['proprietario']) === true) && (strpos($_SESSION['gilda'], ''.$chat_info['proprietario']))))) { ?>
-                        <option value="5"><?php echo gdrcd_filter('out', $MESSAGE['chat']['type'][5]);//invita?></option>
-                        <option value="6"><?php echo gdrcd_filter('out', $MESSAGE['chat']['type'][6]);//caccia?></option>
-                        <option value="7"><?php echo gdrcd_filter('out', $MESSAGE['chat']['type'][7]);//elenco?></option>
-                    <?php }//if
-                    ?>
+
+                    <?php if ($chat_privata && ($login_proprietario_chat || $gilda_proprietaria_chat)) { ?>
+
+                        <!-- Invita in chat -->
+                        <option value="5"><?php echo gdrcd_filter('out', $MESSAGE['chat']['type'][5]); ?></option>
+
+                        <!-- Espelli dalla chat -->
+                        <option value="6"><?php echo gdrcd_filter('out', $MESSAGE['chat']['type'][6]); ?></option>
+
+                        <!-- Elenco invitati -->
+                        <option value="7"><?php echo gdrcd_filter('out', $MESSAGE['chat']['type'][7]); ?></option>
+
+                    <?php } ?>
                 </select>
 
-                <span >Tipo</span>
+                <span>Tipo</span>
             </div>
+
+            <!-- Campo TAG/Destinatario -->
             <div class="input_container small">
                 <input type="text" id="tag" name="tag" placeholder="Tag" value="<?=$_SESSION['tag'] ?>">
-                <span >
-                <?php echo gdrcd_filter('out',$MESSAGE['chat']['tag']['info']['tag'].$MESSAGE['chat']['tag']['info']['dst']);
-                if($_SESSION['permessi']>=GAMEMASTER){echo gdrcd_filter('out',$MESSAGE['chat']['tag']['info']['png']);} ?>
-            </span>
+                <span>
+                    <?php
+                        $descrizione_tag = $MESSAGE['chat']['tag']['info']['tag']
+                            . $MESSAGE['chat']['tag']['info']['dst'];
+
+                        if($login_gamemaster){
+                            $descrizione_tag .= $MESSAGE['chat']['tag']['info']['png'];
+                        }
+
+                        echo gdrcd_filter('out', $descrizione_tag);
+                    ?>
+                </span>
             </div>
+
+            <!-- Campo Testo Azione -->
             <div class="input_container big">
                 <input type="text" name="message" id="testo" placeholder="Testo azione">
 
-                <br /><span class="casella_info">
-	                                    <?php echo gdrcd_filter('out', $MESSAGE['chat']['tag']['info']['msg']); ?>
-	                                </span>
+                <br />
+
+                <span class="casella_info">
+                    <?php echo gdrcd_filter('out', $MESSAGE['chat']['tag']['info']['msg']); ?>
+                </span>
+
                 <?php if($PARAMETERS['mode']['chatsave'] == 'ON') { ?>
-                    <span class="casella_info">
-                                        <a href="javascript:void(0);" onClick="window.open('chat_save.proc.php','Log','width=1,height=1,toolbar=no');">
-                                            Salva Chat
-                                        </a>
-                                    </span>
-                <?php }
-                if (REG_ROLE) { ?>
-                    | <a href="javascript:parent.modalWindow('rolesreg', '', 'popup.php?page=chat_pannelli_index&pannello=segnalazione_role');">
-                        Registra giocata
-                    </a>
+                    | <span class="casella_info">
+                        <a href="javascript:void(0);" onClick="window.open('chat_save.proc.php','Log','width=1,height=1,toolbar=no');">
+                            Salva Chat
+                        </a>
+                    </span>
+                <?php } ?>
+
+                <?php if (REG_ROLE) { ?>
+                    | <span class="casella_info">
+                        <a href="javascript:parent.modalWindow('rolesreg', '', 'popup.php?page=chat_pannelli_index&pannello=segnalazione_role');">
+                            Registra giocata
+                        </a>
+                    </span>
                 <?php  } ?>
 
-
-	  | <span id="conta">Caratteri: 0</span>
-            </span>
+	            | <span id="conta">Caratteri: 0</span>
             </div>
+
+            <!-- Tasto invio azione -->
             <div class="input_container invia">
-                <input type="hidden" name="op" id="op" value="send_action">
-                <input type="hidden" name="location" id="location" value="<?=$_REQUEST['dir']  ?>">
                 <button class="casella_chat" id="inviaAzione">Invia</button>
             </div>
+
         </div>
+    </form>
 
-</form>
-    <?php if($PARAMETERS['mode']['skillsystem'] == 'ON' || $PARAMETERS['mode']['dices'] == 'ON') { ?>
-    <form action="/main.php?dir=<?=$_REQUEST['dir'] ?>" method="post" id="statForm">
+    <?php if ($skills || $stats || $dice || $items) { ?>
 
-        <div class="chat_text chat_row">
-                    <?php if($PARAMETERS['mode']['skillsystem'] == 'ON') { ?>
-                        <div class="casella_chat">
-                            <?php $result = gdrcd_query("SELECT id_abilita, nome FROM abilita WHERE id_razza=-1 OR id_razza IN (SELECT id_razza FROM personaggio WHERE nome = '".$_SESSION['login']."') ORDER BY nome", 'result'); ?>
-                            <select name="id_ab" id="id_ab">
-                                <option value="no_skill"></option>
-                                <?php while($row = gdrcd_query($result, 'fetch')) { ?>
-                                    <option value="<?php echo $row['id_abilita']; ?>">
-                                        <?php echo gdrcd_filter('out', $row['nome']); ?>
-                                    </option>
-                                <?php }//while
-                                ?>
-                            </select>
-                            <br /><span class="casella_info"><?php echo gdrcd_filter('out', $MESSAGE['chat']['commands']['skills']); ?></span>
-                        </div>
-                        <div class="casella_chat">
-                            <select name="id_stats" id="id_stats">
-                                <option value="no_stats"></option>
-                                <?php
-                                /** * Questo modulo aggiunge la possibilità di eseguire prove col dado e caratteristica.
-                                 * Pertanto sono qui elencate tutte le caratteristiche del pg.
-                                 * @author Blancks
-                                 */
-                                foreach($PARAMETERS['names']['stats'] as $id_stats => $name_stats) {
-                                    if(is_numeric(substr($id_stats, 3))) {
-                                        ?>
-                                        <option value="stats_<?php echo substr($id_stats, 3); ?>"><?php echo $name_stats; ?></option>
-                                        <?php
-                                    }
-                                }
-                                ?>
-                            </select>
-                            <br /><span class="casella_info"><?php echo gdrcd_filter('out', $MESSAGE['chat']['commands']['stats']); ?></span>
-                        </div>
-                        <?php
-                    } else {
-                        echo '<input type="hidden" name="id_ab" id="id_ab" value="no_skill">';
-                    }
-                    if($PARAMETERS['mode']['dices'] == 'ON') { ?>
-                        <div class="casella_chat">
-                            <select name="dice" id="dice">
-                                <option value="no_dice"></option>
-                                <?php
-                                /** * Tipi di dado personalizzati da config
-                                 * @author Blancks
-                                 */
-                                foreach($PARAMETERS['settings']['skills_dices'] as $dice_name => $dice_value) { ?>
-                                    <option
-                                            value="<?php echo $dice_value; ?>"><?php echo $dice_name; ?></option>
-                                    <?php
-                                }
-                                ?>
-                            </select>
-                            <br /><span class="casella_info"><?php echo gdrcd_filter('out', $MESSAGE['chat']['commands']['dice']); ?></span>
-                        </div>
-                        <?php
-                    } else {
-                        echo '<input type="hidden" name="dice" id="dice" value="no_dice">';
-                    }
-                    if($PARAMETERS['mode']['skillsystem'] == 'ON') { ?>
-                        <div class="casella_chat">
-                            <?php
-                            $result = gdrcd_query("SELECT clgpersonaggiooggetto.id_oggetto, oggetto.nome, clgpersonaggiooggetto.cariche FROM clgpersonaggiooggetto JOIN oggetto ON clgpersonaggiooggetto.id_oggetto = oggetto.id_oggetto WHERE clgpersonaggiooggetto.nome = '".$_SESSION['login']."' AND posizione > 0 ORDER BY oggetto.nome", 'result'); ?>
-                            <select name="id_item" id="id_item">
-                                <option value="no_item"></option>
-                                <?php while($row = gdrcd_query($result, 'fetch')) { ?>
-                                    <option value="<?php echo $row['id_oggetto'];?>">
-                                        <?php echo $row['nome']; ?>
-                                    </option>
-                                    <?php
-                                }//while
-                                gdrcd_query($result, 'free');
-                                ?>
-                            </select>
-                            <br /><span class="casella_info"><?php echo gdrcd_filter('out', $MESSAGE['chat']['commands']['item']); ?></span>
-                        </div>
-                        <?php
-                    } else {
-                        echo '<input type="hidden" name="id_item" id="id_item" value="no_item">';
-                    } ?>
+        <!-- Form per tiro caratteristica/abilità/dadi e utilizzo oggetti in chat -->
+        <form action="pages/chat/ajax.php?op={operation}" method="post" id="statForm">
+            <div class="chat_text chat_row">
+
+                <?php if ($skills) { ?>
+
+                    <!-- Tendina abilità -->
                     <div class="casella_chat">
-                        <input type="submit" value="<?php echo gdrcd_filter('out', $MESSAGE['interface']['forms']['submit']); ?>" onclick="inviaStat()" />
-                        <input type="hidden" name="op" id="opstat" value="take_action">
+                        <select name="id_ab" id="id_ab">
+
+                            <option value="no_skill"></option>
+
+                            <?php foreach ($skills as $row) { ?>
+                                <option value="<?php echo $row['id_abilita']; ?>">
+                                    <?php echo gdrcd_filter('out', $row['nome']); ?>
+                                </option>
+                            <?php } ?>
+
+                        </select>
+
+                        <br />
+
+                        <span class="casella_info">
+                            <?php echo gdrcd_filter('out', $MESSAGE['chat']['commands']['skills']); ?>
+                        </span>
                     </div>
-                </form>
+
+                <?php } ?>
+
+                <?php if ($stats) { ?>
+
+                    <!-- Tendina caratteristiche -->
+                    <div class="casella_chat">
+                        <select name="id_stats" id="id_stats">
+
+                            <option value="no_stats"></option>
+
+                            <?php foreach($stats as $row) { ?>
+                                <option value="stats_<?php echo $row['id_stats']; ?>">
+                                    <?php echo $row['nome']; ?>
+                                </option>
+                            <?php } ?>
+
+                        </select>
+
+                        <br />
+
+                        <span class="casella_info">
+                            <?php echo gdrcd_filter('out', $MESSAGE['chat']['commands']['stats']); ?>
+                        </span>
+                    </div>
+
+                <?php } ?>
+
+                <?php if ($dice) { ?>
+
+                    <!-- Tendina dadi -->
+                    <div class="casella_chat">
+                        <select name="dice" id="dice">
+
+                            <option value="no_dice"></option>
+
+                            <?php foreach($dice as $row) { ?>
+                                <option value="<?php echo $row['facce']; ?>">
+                                    <?php echo $row['nome']; ?>
+                                </option>
+                            <?php } ?>
+
+                        </select>
+
+                        <br />
+
+                        <span class="casella_info">
+                            <?php echo gdrcd_filter('out', $MESSAGE['chat']['commands']['dice']); ?>
+                        </span>
+                    </div>
+
+                <?php } ?>
+
+                <?php if ($items) { ?>
+
+                    <!-- Tendina oggetti -->
+                    <div class="casella_chat">
+                        <select name="id_item" id="id_item">
+
+                            <option value="no_item"></option>
+
+                            <?php foreach ($items as $row) { ?>
+                                <option value="<?php echo $row['id_oggetto'];?>">
+                                    <?php echo $row['nome']; ?>
+                                </option>
+                            <?php } ?>
+
+                        </select>
+
+                        <br />
+
+                        <span class="casella_info">
+                            <?php echo gdrcd_filter('out', $MESSAGE['chat']['commands']['item']); ?>
+                        </span>
+                    </div>
+
+                <?php } ?>
+
+                <div class="casella_chat">
+                    <input type="submit" value="<?php echo gdrcd_filter('out', $MESSAGE['interface']['forms']['submit']); ?>" onclick="inviaStat()" />
+                </div>
+
             </div>
+        </form>
 
+    <?php } ?>
 
-</div>
-        <?php } ?>
 </div>
 
 <script type="text/javascript" src="/includes/jquery-3.7.1.min.js"></script>
