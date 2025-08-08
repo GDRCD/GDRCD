fc18943e34b94df9dba9b7cf472a8e63e6f2b6f0
blancks <s.rotondo90@gmail.com>
2025-08-09 01:22:38 +0200

    Aggiunge l'inclusione delle funzioni per la gestione delle chat nel file required.php

--- includes/required.php
+++ includes/required.php
@@ -13,6 +13,11 @@ require_once dirname(__FILE__) . '/DbMigration/DbMigration.class.php';
 require_once(dirname(__FILE__) . '/../vocabulary/' . $PARAMETERS['languages']['set'] . '.vocabulary.php');
 require_once(dirname(__FILE__) . '/functions.inc.php');
 
+// include le funzioni per le chat
+require_once(dirname(__FILE__) . '/functions.chat_core.inc.php');
+require_once(dirname(__FILE__) . '/functions.chat_read.inc.php');
+require_once(dirname(__FILE__) . '/functions.chat_write.inc.php');
+
 /** Gestione dei Suoni */
 require_once(dirname(__FILE__) . '/AudioController.class.php');
 
