18ce4f356361ca4b2966e34f6722124b69ba285f
blancks <s.rotondo90@gmail.com>
2025-08-13 00:58:25 +0200

    Aggiunge indice composito nella tabella chat per ottimizzarne le performance

--- db_versions/2025073023_GDRCD57.php
+++ db_versions/2025073023_GDRCD57.php
@@ -7,7 +7,11 @@ class GDRCD57 extends DbMigration
      */
     public function up()
     {
+        // Colonna dice per associare un tipo di dado al lancio di una specifica abilità
         gdrcd_query("ALTER TABLE abilita ADD COLUMN dice TINYINT(3) UNSIGNED NULL DEFAULT NULL AFTER car");
+
+        // Indice di ottimizzazione per le chat
+        gdrcd_query("CREATE INDEX idx_chat_stanza_id_ora ON chat (stanza, id, ora)");
     }
 
     /**
@@ -15,6 +19,7 @@ class GDRCD57 extends DbMigration
      */
     public function down()
     {
+        gdrcd_query("DROP INDEX idx_chat_stanza_id_ora ON chat");
         gdrcd_query("ALTER TABLE abilita DROP COLUMN dice");
     }
 }
