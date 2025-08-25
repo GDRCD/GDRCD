0ff16cbd18ec05dbc1eff8adcd4225082a7634e2
blancks <s.rotondo90@gmail.com>
2025-08-25 02:23:19 +0200

    Refactor di `chat_skillsystem.inc.php` per usare le nuovi funzioni specifiche per delegare il compito di eseguire l'operazione in chat per abilità, caratteristiche, dadi e oggetti

--- pages/chat/op/chat_skillsystem.inc.php
+++ pages/chat/op/chat_skillsystem.inc.php
@@ -12,19 +12,60 @@
 // Garantisce che che questo file sia utilizzato unicamente da /pages/chat/ajax.php
 gdrcd_chat_module_allowed('chat');
 
-// Dati in input inviati dal giocatore
-$id_ab = $_POST['id_ab'] ?? '';
-$id_stats = $_POST['id_stats'] ?? '';
-$dice = $_POST['dice'] ?? '';
-$id_item = $_POST['id_item'] ?? '';
+// Valore tendina "Selezione Tiro"
+$selezione_tiro = $_POST['id_selection'] ?? '';
 
-// Prova ad eseguire l'azione richiesta
-$chat_skillsystem_status = gdrcd_chat_use_skillsystem(
-    $id_ab,
-    $id_stats,
-    $dice,
-    $id_item
-);
+switch ($selezione_tiro) {
 
-// Ritorna una risposta al browser formattata in base all'esito dell'operazione precedente
-gdrcd_chat_output($chat_skillsystem_status);
+    // Tiro Abilità
+    case 'skills':
+        // Dati in input dal form correlato
+        $id_ab = $_POST['id_ab'] ?? '';
+
+        // Chiama la funzione responsabile per effettuare la tipologia di tiro
+        $output = gdrcd_chat_use_skill($id_ab);
+        break;
+
+    // Tiro Caratteristica
+    case 'stats':
+        // Dati in input dal form correlato
+        $id_stats = $_POST['id_stats'] ?? '';
+
+        // Chiama la funzione responsabile per effettuare la tipologia di tiro
+        $output = gdrcd_chat_use_stats($id_stats);
+        break;
+
+    // Tiro Dado
+    case 'dice':
+        // Dati in input dal form correlato
+        $dice = $_POST['dice'] ?? '';
+        $dice_number = $_POST['dice_number'] ?? '';
+        $dice_modifier = $_POST['dice_modifier'] ?? '';
+        $dice_threshold = $_POST['dice_threshold'] ?? '';
+
+        // Chiama la funzione responsabile per effettuare la tipologia di tiro
+        $output = gdrcd_chat_use_dice(
+            $dice,
+            $dice_number,
+            $dice_modifier,
+            $dice_threshold
+        );
+        break;
+
+    // Uso oggetti in chat
+    case 'items':
+        // Dati in input dal form correlato
+        $id_item = $_POST['id_item'] ?? '';
+
+        // Chiama la funzione responsabile per effettuare la tipologia di tiro
+        $output = gdrcd_chat_use_item($id_item);
+        break;
+
+    // Tipologia sconosciuta o non selezionata
+    default:
+        $output = gdrcd_chat_status_invalid($MESSAGE['chat']['error']['invalid_skillsystem_type']);
+        break;
+}
+
+// Ritorna una risposta al browser formattata in base all'esito dell'operazione eseguita
+gdrcd_chat_output($output);
