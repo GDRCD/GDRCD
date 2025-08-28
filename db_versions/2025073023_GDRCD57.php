47d22d6ffc9542df8634ffa22455564d4ff36ddf
blancks <s.rotondo90@gmail.com>
2025-08-28 02:13:57 +0200

    Modifica il charset di default della tabella chat in utf8mb4_general_ci

--- db_versions/2025073023_GDRCD57.php
+++ db_versions/2025073023_GDRCD57.php
@@ -7,6 +7,10 @@ class GDRCD57 extends DbMigration
      */
     public function up()
     {
+        // Cambia il charset della tabella chat ad utf8mb4
+        gdrcd_query("ALTER TABLE `chat` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci");
+        gdrcd_query("ALTER TABLE `chat` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci");
+
         // Indice di ottimizzazione per le chat
         gdrcd_query("CREATE INDEX idx_chat_stanza_id_ora ON chat (stanza, id, ora)");
     }
@@ -17,5 +21,9 @@ class GDRCD57 extends DbMigration
     public function down()
     {
         gdrcd_query("DROP INDEX idx_chat_stanza_id_ora ON chat");
+
+        // Ripristina il charset precedente
+        gdrcd_query("ALTER TABLE `chat` CONVERT TO CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci");
+        gdrcd_query("ALTER TABLE `chat` DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci");
     }
 }
