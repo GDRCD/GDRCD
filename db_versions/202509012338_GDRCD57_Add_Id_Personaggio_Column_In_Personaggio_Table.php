15c525f14bde424898688345b21baec34951dc47
blancks <s.rotondo90@gmail.com>
2025-09-01 23:58:25 +0200

    Refactor migrazione per aggiungere la colonna `id_personaggio` nella tabella `personaggio` secondo la nuova convenzione nomi per le migrations

--- db_versions/202509012338_GDRCD57_Add_Id_Personaggio_Column_In_Personaggio_Table.php
+++ db_versions/202509012338_GDRCD57_Add_Id_Personaggio_Column_In_Personaggio_Table.php
@@ -1,15 +1,15 @@
 <?php
 
-class GDRCD57_Add_Personaggio_Id extends DbMigration
+class GDRCD57_Add_Id_Personaggio_Column_In_Personaggio_Table extends DbMigration
 {
     /**
      * @inheritDoc
      */
     public function up()
     {
-        gdrcd_query("ALTER TABLE `personaggio` ADD COLUMN `id` INT UNSIGNED NOT NULL AUTO_INCREMENT FIRST, ADD UNIQUE KEY(`id`)");
+        gdrcd_query("ALTER TABLE `personaggio` ADD COLUMN `id_personaggio` INT UNSIGNED NOT NULL AUTO_INCREMENT FIRST, ADD UNIQUE KEY(`id_personaggio`)");
         gdrcd_query("ALTER TABLE `personaggio` DROP PRIMARY KEY");
-        gdrcd_query("ALTER TABLE `personaggio` ADD PRIMARY KEY (`id`)");
+        gdrcd_query("ALTER TABLE `personaggio` ADD PRIMARY KEY (`id_personaggio`)");
         gdrcd_query("ALTER TABLE `personaggio` ADD UNIQUE KEY(`nome`)");
     }
 
@@ -20,6 +20,7 @@ class GDRCD57_Add_Personaggio_Id extends DbMigration
     {
         gdrcd_query("ALTER TABLE `personaggio` DROP PRIMARY KEY");
         gdrcd_query("ALTER TABLE `personaggio` DROP COLUMN `id`");
+        gdrcd_query("ALTER TABLE `personaggio` DROP INDEX `nome`");
         gdrcd_query("ALTER TABLE `personaggio` ADD PRIMARY KEY (`nome`)");
     }
 }
