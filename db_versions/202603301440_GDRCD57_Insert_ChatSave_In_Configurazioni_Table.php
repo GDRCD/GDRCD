67acc7c9109141bddbb04d9f37fd679e7e461d5e
anneth <74818550+annamereu@users.noreply.github.com>
2026-03-30 14:46:22 +0200

    salva chat
    Ho modificato il salva chat in modo da utilizzare gdrcd_chat_message_handler. Inoltre, ho aggiunto due parametri in configurazione che gestisce sia il tempo di salvataggio espresso in ore che la possibilità o meno di controllare se chi sta salvando la chat è presente nella giocata

---
+++ db_versions/202603301440_GDRCD57_Insert_ChatSave_In_Configurazioni_Table.php
@@ -0,0 +1,18 @@
+<?php
+
+class Insert_ChatSave_In_Configurazioni_Table  extends DbMigration
+{
+    public function up() {
+        // Inserimento parametri su tabella configurazioni
+       
+        gdrcd_query("INSERT INTO `configurazioni` (`id`, `tipo`, `categoria`, `ordinamento`, `opzioni`, `default`, `parametro`, `valore`, `descrizione`, `input`) VALUES (4, 'string', 'salva_chat', 1, 'si,no', 'no', 'controllo_salvataggio', 'si', 'Controlla che chi salva la chat abbia mandato un\'azione in chat', 'select')");
+        gdrcd_query("INSERT INTO `configurazioni` (`id`, `tipo`, `categoria`, `ordinamento`, `opzioni`, `default`, `parametro`, `valore`, `descrizione`, `input`) VALUES (5, 'string', 'salva_chat', 2, '', '2', 'tempo_salvataggio', '2', 'Imposta il numero massimo di ore scaricabili con il salva chat', 'input')");
+   
+
+    }
+
+    public function down() {
+        // Rimozione tabelle create in up()
+        gdrcd_query("DROP TABLE IF EXISTS `configurazioni`");
+    }
+}
