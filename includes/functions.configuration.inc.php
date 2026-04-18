b19c2fbf396fdaddbccec96a6a8800b917277a67
Salvatore <s.rotondo90@gmail.com>
2026-04-18 08:39:22 +0200

    Aggiunta documentazione alle nuove funzioni del modulo caching

--- includes/functions.configuration.inc.php
+++ includes/functions.configuration.inc.php
@@ -54,16 +54,33 @@ function gdrcd_configuration_set($parametro, $value)
     );
 }
 
+/**
+ * Restituisce il percorso completo del file di cache della configurazione
+ *
+ * @return string Il path del file di cache
+ */
 function gdrcd_configuration_cache_filename()
 {
     return GDRCD_PATH . 'cache/configuration.cache.php';
 }
 
+/**
+ * Verifica se esiste il file di cache della configurazione
+ *
+ * @return bool True se il file di cache esiste, false altrimenti
+ */
 function gdrcd_configuration_cache_exist()
 {
     return file_exists(gdrcd_configuration_cache_filename());
 }
 
+/**
+ * Crea il file di cache della configurazione
+ * Recupera tutte le configurazioni dal database e le memorizza in un file di cache PHP.
+ *
+ * @throws Exception Se non può creare la cartella o il file di cache
+ * @return void
+ */
 function gdrcd_configuration_cache_create()
 {
     $configurations = gdrcd_stmt_all('SELECT categoria, parametro, valore, `default` FROM configurazioni');
@@ -92,6 +109,14 @@ function gdrcd_configuration_cache_create()
     }
 }
 
+/**
+ * Recupera il contenuto della cache delle configurazioni
+ *
+ * Utilizza una variabile statica per memorizzare la cache e evitare letture multiple.
+ * Se la cache non esiste, restituisce un array vuoto.
+ *
+ * @return array<string, string> Array associativo con le configurazioni nel formato "categoria.parametro" => "valore"
+ */
 function gdrcd_configuration_cache_get()
 {
     static $cache = null;
